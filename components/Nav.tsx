88d45f855d588f9a417a3a2a083ccf12ff017071
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-07-09 22:58:33 +0200

    initial commit

---
+++ components/Nav.tsx
@@ -0,0 +1,67 @@
+interface NavProps {
+    loggedIn: boolean
+}
+//TODO: MAKE THIS MOBILE FRIENDLY!!
+export default function Nav({ loggedIn }: NavProps) {
+    // const menus = [
+    //     { name: "Home", href: "/" },
+    // ];
+
+    const loggedInMenus = [
+        { name: "Accounts", href: "/auth/accounts" },
+        { name: "Sell", href: "/auth/sell" },
+        { name: "Profile", href: "/auth/profile" },
+        { name: "Logout", href: "/logout" },
+    ];
+
+    const nonLoggedInMenus = [
+        { name: "Login", href: "/   " },
+        { name: "SignUp", href: "/signup" },
+    ]
+
+    return (
+        <div class="bg-white max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div class="text-2xl  ml-1 font-bold">
+                Debit Llama
+            </div>
+            <ul class="flex gap-6">
+                {/* {menus.map((menu) => (
+                    <li>
+                        <a
+                            href={menu.href}
+                            class="text-gray-500 hover:text-gray-700 py-1 border-gray-500"
+                        >
+                            {menu.name}
+                        </a>
+                    </li>
+                ))} */}
+
+                {
+                    loggedIn ? (
+                        loggedInMenus.map((menu) => (
+                            <li>
+                                <a
+                                    href={menu.href}
+                                    class="text-gray-500 hover:text-gray-700 py-1 border-gray-500"
+                                >
+                                    {menu.name}
+                                </a>
+                            </li>
+                        ))
+                    ) : (
+                        nonLoggedInMenus.map((menu) => (
+                            <li>
+                                <a
+                                    href={menu.href}
+                                    class="text-gray-500 hover:text-gray-700 py-1 border-gray-500"
+                                >
+                                    {menu.name}
+                                </a>
+                            </li>
+                        ))
+                    )
+                }
+            </ul>
+        </div>
+    );
+}
---
+++ routes/_middleware.ts
@@ -0,0 +1,39 @@
+// deno-lint-ignore-file no-explicit-any
+import { MiddlewareHandlerContext } from "$fresh/server.ts";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { getCookies } from "$std/http/cookie.ts";
+
+export interface State {
+  token: string | null;
+  userid: string | null;
+  supabaseClient: SupabaseClient<any, "public", any>;
+}
+
+export async function handler(
+  req: Request,
+  ctx: MiddlewareHandlerContext<State>,
+) {
+  const client = createClient(
+    Deno.env.get("SUPABASE_URL") || "",
+    Deno.env.get("SUPABASE_KEY") || "",
+  );
+
+  ctx.state.supabaseClient = client;
+
+  const supaCreds = getCookies(req.headers)["supaLogin"];
+
+  if (!supaCreds) {
+    return ctx.next();
+  }
+
+  const { error, data: { user } } = await client.auth.getUser(supaCreds);
+
+  if (error) {
+    ctx.state.token = null;
+  } else {
+    ctx.state.token = supaCreds;
+    ctx.state.userid = user.id;
+  }
+
+  return await ctx.next();
+}
