427eece0e10a59189e1795d78dd32d3f15adbccb
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-07-11 01:31:56 +0200

    added footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+
 import { Head } from "$fresh/runtime.ts";
 import type { ComponentChildren } from "preact";
 import Nav from "./Nav.tsx";
@@ -12,11 +13,23 @@ export default function Layout(props: LayoutProps) {
     <>
       <Head>
         <title>DebitLlama</title>
+        <link rel="stylesheet" href="/styles.css" />
+        <script src="/zxcvbn.js"></script>
       </Head>
-      <Nav active="/" loggedIn={props.isLoggedIn} />
+      <Nav loggedIn={props.isLoggedIn} />
       <div class="p-4 mx-auto max-w-screen-md">
         {props.children}
       </div>
+      <footer
+        class="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+        <div class="p-4 text-center text-neutral-700 dark:text-neutral-200 flex flex-col">
+          © ZKP Tech Solutions Ltd 2023 Copyright {" "}
+          <a
+            class="text-gray-500 hover:text-gray-700 py-1 border-gray-500"
+            href="/app/feedback"
+          >Feedback</a>
+        </div>
+      </footer>
     </>
   );
 }
