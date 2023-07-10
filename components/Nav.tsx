8f6a1e784be0ef4095aa278b6d06bb7700c0538f
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-07-11 01:46:03 +0200

    added logo to nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -18,8 +18,8 @@ export default function Nav({ loggedIn }: NavProps) {
 
     return (
         <div class="bg-white max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <div class="text-2xl  ml-1 font-bold">
-                Debit Llama
+            <div class="text-2xl  ml-1 font-bold flex flex-row">
+                <img src="/logo.svg" width="35" class={"mr-3"} />{" "}Debit Llama
             </div>
             <ul class="flex gap-6">
                 {
@@ -28,7 +28,7 @@ export default function Nav({ loggedIn }: NavProps) {
                             <li>
                                 <a
                                     href={menu.href}
-                                    class="text-gray-500 hover:text-gray-700 py-1 border-gray-500"
+                                    class="text-lg text-gray-500 hover:text-gray-700 py-1 border-gray-500"
                                 >
                                     {menu.name}
                                 </a>
@@ -39,7 +39,7 @@ export default function Nav({ loggedIn }: NavProps) {
                             <li>
                                 <a
                                     href={menu.href}
-                                    class="text-gray-500 hover:text-gray-700 py-1 border-gray-500"
+                                    class="text-lg text-gray-500 hover:text-gray-700 py-1 border-gray-500"
                                 >
                                     {menu.name}
                                 </a>
