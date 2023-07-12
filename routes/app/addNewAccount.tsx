b48d8df22e4f13b4f0e747a472b512c9e6c7bca0
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-07-13 01:02:51 +0200

    passing environment to front end

--- routes/app/addNewAccount.tsx
+++ routes/app/addNewAccount.tsx
@@ -4,6 +4,11 @@ import Layout from "../../components/Layout.tsx";
 import AccountCreatePageForm from "../../islands/accountCreatePageForm.tsx";
 import { State } from "../_middleware.ts";
 
+const ethEncryptPublicKey = Deno.env.get("ETHENCRYPTPUBLICKEY") || "";
+const directdebitContract_donau = Deno.env.get("DIRECTDEBITCONTRACT_DONAUTESTNET") || "";
+const usdtm_donau = Deno.env.get("USDTM_DONAUTESTNET") || "";
+
+
 export const handler: Handlers<any, State> = {
     async GET(_req, ctx) {
         const headers = new Headers();
@@ -14,18 +19,29 @@ export const handler: Handlers<any, State> = {
             headers.set("location", "/app/profile");
             return new Response(null, { status: 303, headers })
         } else {
-            return ctx.render({ ...ctx.state })
+            return ctx.render({ ...ctx.state, ethEncryptPublicKey, directdebitContract_donau, usdtm_donau })
         }
     },
-    POST(_req, ctx) {
-        return ctx.render({ ...ctx.state })
+    async POST(_req, ctx) {
+        // Here I accept json
+        const json = await _req.json();
+        // Adda  verificaiton to check if the request contains all the needed things!
+        //TODO: verify the contents are good.
+        // here I need to query the blockchain to make sure the commitment exists!
+
+        //Then I return accepted
+        return new Response(null, { status: 202 })
     }
 }
 
 export default function AddNewAccount(props: PageProps) {
     return <Layout isLoggedIn={props.data.token}>
         <div class="container mx-auto py-8">
-            <AccountCreatePageForm></AccountCreatePageForm>
+            <AccountCreatePageForm
+                mockERC20Address_donau={props.data.usdtm_donau}
+                directdebitContract_donau={props.data.directdebitContract_donau}
+                ethEncryptPublicKey={props.data.ethEncryptPublicKey}
+            ></AccountCreatePageForm>
         </div>
     </Layout>
 }
