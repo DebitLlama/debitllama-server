053208bebe99e2f54d468f8ead01b98a5ec0bf0b
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-07-13 01:04:04 +0200

    create page submit implemented (WIP)

--- islands/accountCreatePageForm.tsx
+++ islands/accountCreatePageForm.tsx
@@ -2,8 +2,10 @@ import { useState } from 'preact/hooks';
 
 import CurrencySelectDropdown from "./CurrencySelectDropdown.tsx";
 import AccountPasswordInput from "./accountPasswordInput.tsx";
-import { isEthereumUndefined, redirectToMetamask, requestAccounts } from "../lib/frontend/web3.ts";
-import { newAccountSecrets } from '../lib/frontend/directdebitlib.ts';
+import { ChainIds, NetworkNames, approveSpend, availableNetworks, chainIdFromNetworkName, depositEth, depositToken, fetchAbi, getAllowance, getContract, getDirectDebitContractAddress, handleNetworkSelect, parseEther, requestAccounts } from "../lib/frontend/web3.ts";
+import { decodeAccountSecrets, ethEncryptData, newAccountSecrets, toNoteHex } from '../lib/frontend/directdebitlib.ts';
+import { aesEncryptData } from '../lib/frontend/encryption.ts';
+import { saveAccountData } from '../lib/frontend/fetch.ts';
 
 const strength = [
     "Worst ☹",
@@ -13,12 +15,38 @@ const strength = [
     "Strong ☻"
 ]
 
-export default function AccountCreatePageForm() {
+export type SelectableCurrency =
+    { name: string, native: boolean, contractAddress: string }
+
+
+const ethereumCurrencies = [
+    { name: "ETH", native: true, contractAddress: "" },
+    { name: "USDC", native: false, contractAddress: "" }
+]
+
+const bittorrentCurrencies = [{ name: "BTT", native: true, contractAddress: "" }, { name: "USDT", native: false, contractAddress: "" }]
+
+interface AccountCreatePageFormProps {
+    ethEncryptPublicKey: string,
+    directdebitContract_donau: string,
+    mockERC20Address_donau: string
+}
+
+export default function AccountCreatePageForm(props: AccountCreatePageFormProps) {
+    const [name, setName] = useState("");
     const [password, setPassword] = useState("");
     const [passwordAgain, setPasswordAgain] = useState("");
     const [passwordStrengthNotification, setPasswordStrengthNotification] = useState("");
     const [passwordMatchError, setPasswordMatchError] = useState("");
     const [passwordScore, setPasswordScore] = useState(0);
+
+    const [createAccountButtonText, setCreateAccountButtonText] = useState("Create Account");
+    const [selectedNetwork, setSelectedNetwork] = useState(availableNetworks[0]);
+    const [selectableCurrencyArray, setSelectableCurrencyArray] = useState<SelectableCurrency[]>(bittorrentCurrencies);
+
+    const [selectedCurrency, setSelectedCurrency] = useState<SelectableCurrency>(selectableCurrencyArray[0]);
+
+    const [depositAmount, setDepositAmount] = useState("");
 
     function setPasswordAndCheck(to: string) {
         if (to === "") {
@@ -64,44 +92,153 @@ export default function AccountCreatePageForm() {
         return false;
     }
 
+    function setSelectedCurrencyHook(to: SelectableCurrency) {
+        setSelectedCurrency(to);
+        if (!to.native) {
+            setCreateAccountButtonText(`Create Account (ERC-20)`)
+        } else {
+            setCreateAccountButtonText("Create Account")
+        }
+    }
+
+    async function setUpAccount() {
+        const note = newAccountSecrets();
+        const secrets = decodeAccountSecrets(note);
+        const commitment = toNoteHex(secrets.commitment);
+        //Password encrypt the note!
+        const passwdEncryptedNote = await aesEncryptData(note, password);
+        //Encrypt the note with a public key
+        const encryptedNote = await ethEncryptData(props.ethEncryptPublicKey, passwdEncryptedNote);
+
+        return { encryptedNote, commitment };
+    }
+
+    function handleError(msg: string) {
+        console.log(msg);
+    }
+
+    async function handleTokenDeposit(
+        contractAddress: string,
+        virtualaccount: {
+            encryptedNote: any;
+            commitment: any;
+        },
+        erc20Contract: string,
+        chainId: string
+    ) {
+        const depositTx = await depositToken(
+            contractAddress,
+            virtualaccount.commitment,
+            depositAmount,
+            erc20Contract,
+            virtualaccount.encryptedNote)
+
+        if (depositTx !== undefined) {
+            await depositTx.wait().then(async (receipt2: any) => {
+                if (receipt2.status === 1) {
+                    // Save the data on the backend!
+                    const status = await saveAccountData(chainId, virtualaccount.commitment, name);
+                    //TODO: REdirect to a new page
+                    // returns 202 accepted on success, then I need to redirect
+                }
+            })
+        }
+    }
+
+
     async function onSubmitForm(event: any) {
         event.preventDefault();
         // I check if I can find a wallet
-        const web3Undefined = isEthereumUndefined();
-        if (web3Undefined) {
-            redirectToMetamask();
+        const chainId = chainIdFromNetworkName[selectedNetwork as NetworkNames];
+
+        const provider = await handleNetworkSelect(chainId, handleError);
+
+        if (!provider) {
             return;
         }
-        await requestAccounts();
-
-        // I generate the secret here
-
-        const note = newAccountSecrets();
-        console.log(note);
 
 
+        const address = await requestAccounts();
+        const virtualaccount = await setUpAccount();
+        const contractAddress = getDirectDebitContractAddress[chainId as ChainIds];
 
-        // Encrypt the secret (I need to generate the public key too and import eth-sig-utils)
+        if (!selectedCurrency?.native) {
+            //Approve spending, Then do the deposit
+
+            const erc20Contract = await getContract(
+                provider,
+                selectedCurrency.contractAddress,
+                "/ERC20.json");
 
 
-        // I submit it to the blockchain 
+            const allowance: bigint = await getAllowance(erc20Contract, address, contractAddress);
 
-        // Then after the transaction is confirmed I will save the transaction in supabase on the backend
+            if (allowance >= parseEther(depositAmount)) {
+                // Just deposit
+                await handleTokenDeposit(contractAddress, virtualaccount, erc20Contract, chainId)
+            } else {
+                // Add allowance and then deposit 
+                const approveTx = await approveSpend(
+                    erc20Contract,
+                    contractAddress,
+                    depositAmount);
 
-        // and I redirect to the virtual account's page where it show the details and can be withdrawn or topped up!
+                if (approveTx !== undefined) {
+                    await approveTx.wait().then(async (receipt: any) => {
+                        if (receipt.status === 1) {
+                            await handleTokenDeposit(contractAddress, virtualaccount, erc20Contract, chainId)
+                        }
+                    })
+                }
+            }
+        } else {
+            // Do the deposit
+            const contract = await getContract(
+                provider,
+                contractAddress,
+                "/DirectDebit.json");
 
+            const tx = await depositEth(
+                contract,
+                virtualaccount.commitment,
+                depositAmount,
+                virtualaccount.encryptedNote
+            );
 
-        // return true;
+            if (tx !== undefined) {
+                await tx.wait().then(async (receipt: any) => {
+                    if (receipt.status === 1) {
+                        // Save the data on the backend!
+                        const status = await saveAccountData(chainId, virtualaccount.commitment, name);
+                        //returns 202 accepted on success, then I need to redirect
+                        //TODO: redirect to a new page
+                    }
+                })
+            }
+        }
     }
 
 
     return <form onSubmit={onSubmitForm} class="w-full max-w-sm mx-auto bg-white p-8 rounded-md shadow-md" method="POST">
         <h1 class="text-2xl font-bold mb-6 text-center">New Virtual Account</h1>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Account Name</label>
+            <input value={name} onChange={(event: any) => setName(event.target.value)} required class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                type="text" id="name" name="name" placeholder="" />
+        </div>
 
-        <CurrencySelectDropdown></CurrencySelectDropdown>
+        <CurrencySelectDropdown
+            setSelectedNetwork={setSelectedNetwork}
+            selectedNetwork={selectedNetwork}
+            selectableCurrencyArray={selectableCurrencyArray}
+            setSelectableCurrencyArray={setSelectableCurrencyArray}
+            availableNetworks={availableNetworks}
+            selectedCurrency={selectedCurrency}
+            setSelectedCurrency={setSelectedCurrencyHook}
+        ></CurrencySelectDropdown>
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="amount">Deposit Amount</label>
-            <input required class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+            <input value={depositAmount} onChange={(event: any) => setDepositAmount(event.target.value)} required class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
                 type="number" id="amount" name="amount" placeholder="0" />
         </div>
         <AccountPasswordInput
@@ -115,6 +252,6 @@ export default function AccountCreatePageForm() {
         <button
             disabled={isButtonDisabled()}
             class="w-full bg-indigo-500 text-white text-sm font-bold py-2 px-4 rounded-md  hover:bg-indigo-600 disabled:bg-indigo-100 transition duration-300"
-            type="submit">Create Account</button>
+            type="submit">{createAccountButtonText}</button>
     </form>
 }
