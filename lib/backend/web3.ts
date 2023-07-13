4aec6ea6b9eecdd38702838de593ff9edcb5725e
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-07-13 06:58:24 +0200

    added web3 for backend

--- lib/backend/web3.ts
+++ lib/backend/web3.ts
@@ -1,7 +1,35 @@
-import { ethers } from "$ethers";
+import { ethers, ZeroAddress } from "$ethers";
+import DirectDebitArtifact from "../../static/DirectDebit.json" assert {
+  type: "json",
+};
+import {
+  ChainIds,
+  getDirectDebitContractAddress,
+  rpcUrl,
+} from "../shared/web3.ts";
 
 export function validateAddress(address: string) {
   return ethers.isAddress(address);
 }
 
+export function getProvider(networkId: string) {
+  const url = rpcUrl[networkId as ChainIds];
+  return new ethers.JsonRpcProvider(url);
+}
+
 // I need to implement the server side Contract functions here with ethers js
+export function getContract(provider: any, networkId: string) {
+  const address = getDirectDebitContractAddress[networkId as ChainIds];
+  return new ethers.Contract(
+    address,
+    DirectDebitArtifact.abi,
+    provider,
+  );
+}
+
+export async function getAccount(commitment: string, networkId: string) {
+  const provider = getProvider(networkId);
+  const contract = getContract(provider, networkId);
+  const account = await contract.accounts(commitment);
+  return { account, exists: account.creator !== ZeroAddress };
+}
