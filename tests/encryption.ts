8bc3109d1befffe2f407e60eb539bb754ff37298
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-07-13 03:07:23 +0200

    tests updated

--- tests/encryption.ts
+++ tests/encryption.ts
@@ -1,11 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.80.0/testing/asserts.ts";
-import { decryptData, encryptData } from "../lib/frontend/encryption.ts";
+import { aesDecryptData, aesEncryptData } from "../lib/frontend/encryption.ts";
 
 Deno.test("Testing password encryption and decryption", async () => {
   const note = "THISISASTRINGTOENCRYPT";
   const passwd = "password";
 
-  const encryptedData = await encryptData(note, passwd);
-  const decryptedData = await decryptData(encryptedData, passwd);
+  const encryptedData = await aesEncryptData(note, passwd);
+  const decryptedData = await aesDecryptData(encryptedData, passwd);
   assertEquals(decryptedData, note);
 });
