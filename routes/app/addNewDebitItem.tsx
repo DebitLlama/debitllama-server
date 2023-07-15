390a686627a6fd170fe1f91aadb96205b52cb7a3
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-07-15 15:36:50 +0200

    created addMewDebitItem page

---
+++ routes/app/addNewDebitItem.tsx
@@ -0,0 +1,72 @@
+import Layout from "../../components/Layout.tsx";
+import { State } from "../_middleware.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import AddNewDebitItemPageForm from "../../islands/addNewDebitItemPageForm.tsx";
+
+
+
+export const handler: Handlers<any, State> = {
+    async GET(_req, ctx) {
+        const headers = new Headers();
+
+        const userid = ctx.state.userid;
+
+        // Get the data and use it to populate the fields!
+        const { data: profileData, error: profileError } = await ctx.state.supabaseClient.from("Profiles").select().eq("userid", userid);
+
+        if (profileData === null || profileData.length === 0) {
+            headers.set("location", "/app/profile");
+            return new Response(null, { status: 303, headers })
+        }
+
+        return ctx.render({ ...ctx.state, creatorAddress: profileData[0].walletaddress })
+    },
+    async POST(_req, ctx) {
+        const headers = new Headers();
+        const userid = ctx.state.userid;
+
+        const form = await _req.formData();
+
+        const { data: profileData, error: profileError } = await ctx.state.supabaseClient.from("Profiles").select().eq("userid", userid);
+
+        if (profileData === null || profileData.length === 0) {
+            headers.set("location", "/app/profile");
+            return new Response(null, { status: 303, headers })
+        }
+        const name = form.get("name") as string;
+        const network = form.get("network") as string;
+        const currency = form.get("currency") as string;
+        const debitType = form.get("debitType") as string;
+        const pricing = form.get("pricing") as string;
+        const maxAmount = form.get("maxamount") as string;
+        const debitTimes = form.get("debitTimes") as string;
+        const debitInterval = form.get("debitInterval") as string;
+        const redirectto = form.get("redirectto") as string;
+
+        await ctx.state.supabaseClient.from("Items").insert({
+            created_at: new Date().toISOString(),
+            payee_id: userid,
+            payee_address: profileData[0].walletaddress,
+            currency,
+            max_price: maxAmount,
+            debit_times: debitTimes,
+            debit_interval: debitInterval,
+            redirect_url: redirectto,
+            pricing,
+            network,
+            name,
+            debitType
+        })
+
+        headers.set("location", "/app/debitItems");
+        return new Response(null, { status: 303, headers })
+    }
+}
+
+export default function AddNewDebitItem(props: PageProps) {
+    return <Layout isLoggedIn={props.data.token}>
+        <div class="container mx-auto py-8">
+            <AddNewDebitItemPageForm creatorAddress={props.data.creatorAddress}></AddNewDebitItemPageForm>
+        </div>
+    </Layout>
+}
