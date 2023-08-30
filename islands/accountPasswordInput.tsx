21db836e51c69a828901f0c6a55d1e68e2e9fba5
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-08-30 17:38:01 +0200

    updated confirm password element name

--- islands/accountPasswordInput.tsx
+++ islands/accountPasswordInput.tsx
@@ -26,7 +26,7 @@ export default function AccountPasswordInput(props: AccountPasswordInputProps) {
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="confirm-password">Confirm Password</label>
             <input value={props.passwordAgain} onKeyUp={(event: any) => props.setPasswordAgain(event.target.value)} onChange={(event: any) => props.setPasswordAgain(event.target.value)} required data-lpignore="true" autocomplete={"off"} class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                type="password" id="confirm-password" name="confirm-password" placeholder="********" />
+                type="password" id="confirm-password" name="confirmpassword" placeholder="********" />
             <p class="text-red-800">{props.passwordMatchError}</p>
         </div>
         <div class="mb-4">
