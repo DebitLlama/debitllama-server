b3f3723d29ddd9676db680774d4c77067103e1c9
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-02 21:14:52 +0200

    feat: handler to save payment intent

---
+++ routes/app/savePaymentIntent.ts
@@ -0,0 +1,72 @@
+import { Handlers } from "$fresh/server.ts";
+import { estimateRelayerGas } from "../../lib/backend/web3.ts";
+import { PaymentIntentStatus } from "../../lib/paymentIntentStatus.ts";
+import { State } from "../_middleware.ts";
+
+export const handler: Handlers<any, State> = {
+  async POST(_req, ctx) {
+    const json = await _req.json();
+
+    const userid = ctx.state.userid;
+    const item_button_id = json.button_id;
+    const proof = json.proof;
+    const publicSignals = json.publicSignals;
+    const maxDebitAmount = json.maxDebitAmount;
+    const debitTimes = json.debitTimes;
+    const debitInterval = json.debitInterval;
+    const paymentIntent = json.paymentIntent;
+    const commitment = json.commitment;
+
+    // Get the debit item using th button_id
+    const { data: itemData, error: itemError } = await ctx.state.supabaseClient
+      .from("Items").select().eq("button_id", item_button_id);
+
+
+    if (itemData === null || itemData.length === 0) {
+      return new Response(null, { status: 500 });
+    }
+
+    const { data: accountData, error: accountError } = await ctx.state
+      .supabaseClient.from("Accounts").select().eq("commitment", commitment);
+
+    if (accountData === null || accountData.length === 0) {
+      return new Response(null, { status: 500 });
+    }
+
+    const estimatedGas = await estimateRelayerGas({
+      proof,
+      publicSignals,
+      payeeAddress: itemData[0].payee_address,
+      maxDebitAmount,
+      actualDebitedAmount: maxDebitAmount,
+      debitTimes,
+      debitInterval,
+    }, itemData[0].network);
+
+    // Now I save it to the database and return ok
+    const { data, error } = await ctx.state.supabaseClient.from(
+      "PaymentIntents",
+    ).insert({
+      created_at: new Date().toISOString(),
+      creator_user_id: userid,
+      payee_user_id: itemData[0].payee_id,
+      account_id: accountData[0].id,
+      payee_address: itemData[0].payee_address,
+      maxDebitAmount: maxDebitAmount,
+      debitTimes,
+      debitInterval,
+      paymentIntent,
+      commitment,
+      estimatedGas: estimatedGas.toString(),
+      statusText: PaymentIntentStatus.CREATED,
+      lastPaymentDate: null,
+      nextPaymentDate: null,
+      pricing: itemData[0].pricing,
+    });
+    if (error !== null) {
+      return new Response(null, { status: 500 });
+    }
+
+    return new Response(null, { status: 200 });
+  },
+};
