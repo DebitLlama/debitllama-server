04933ce26b41b58f8740f9692950acbf6254cbe1
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-02 21:10:11 +0200

    updated checkout display with tooltip

--- components/BuyPageLayout.tsx
+++ components/BuyPageLayout.tsx
@@ -18,7 +18,7 @@ function isDynamic(pricing: string) {
 
 function getDebitIntervalText(debitInterval: number, debitTimes: number) {
     if (debitTimes === 1) {
-        return "";
+        return "Unspecified";
     }
     if (debitInterval === 0) {
         return "Unspecified"
@@ -62,18 +62,47 @@ export default function BuyPageLayout(props: BuyPagelayoutProps) {
                 <div class="p-3 w-full"  >
 
                     <div class="text-center"><h1 class="text-2xl font-bold mb-2">Checkout</h1></div>
-                    <div class="border flex flex-row flex-wrap  p-3 justify-between rounded-xl" style="background-color:white;">
-                        <div class="flex flex-col">
-                            <span class="p-2 font-light text-xl">{props.item.name}</span>
-                            <span class="p-2 font-semibold ">Approved amount: {props.item.maxPrice} {props.item.currency.name}</span>
+                    <div class="flex p-3 rounded-xl" style="background-color:white;">
+                        <table class="table-fixed w-full">
+                            <thead>
+                                <tr> <th></th>
+                                    <th></th>
+                                    <th></th></tr>
 
-                            <span class="p-2">{networkName}</span>
-                        </div>
-                        <div class="flex flex-col">
-                            <span class="p-2 font-light font-sans antialiased">{props.item.pricing} {"Priced "} {" Payment"}</span>
-                            <span class="p-2 font-sans text-slate-600">Approving {props.item.debitTimes} payment{props.item.debitTimes === 1 ? "" : "s"}</span>
-                            <span class="p-2 font-sans text-slate-600">{getDebitIntervalText(props.item.debitInterval, props.item.debitTimes)}</span>
-                        </div>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <UnderlinedTd extraStyles="text-slate-400 dark:text-slate-200 text-sm" >Subscription:</UnderlinedTd>
+                                    <UnderlinedTd extraStyles="font-semibold">{props.item.name}</UnderlinedTd>
+                                    <UnderlinedTd extraStyles=""><Tooltip message="The name of subscription"></Tooltip></UnderlinedTd>
+                                </tr>
+                                <tr>
+                                    <UnderlinedTd extraStyles="text-slate-400 dark:text-slate-200 text-sm" >Approved Payment:</UnderlinedTd>
+                                    <UnderlinedTd extraStyles="font-semibold" >{props.item.maxPrice} {props.item.currency.name}</UnderlinedTd>
+                                    <UnderlinedTd extraStyles=""><Tooltip message="The maximum amount that can be debited from the account"></Tooltip></UnderlinedTd>
+                                </tr>
+                                <tr>
+                                    <UnderlinedTd extraStyles="text-slate-400 dark:text-slate-200 text-sm">Network:</UnderlinedTd>
+                                    <UnderlinedTd extraStyles="font-semibold" >{networkName}</UnderlinedTd>
+                                    <UnderlinedTd extraStyles=""><Tooltip message="The network used for this payment"></Tooltip></UnderlinedTd>
+                                </tr>
+                                <tr>
+                                    <UnderlinedTd extraStyles="text-slate-400 dark:text-slate-200 text-sm">Pricing:</UnderlinedTd>
+                                    <UnderlinedTd extraStyles="font-semibold" >{props.item.pricing}</UnderlinedTd>
+                                    <UnderlinedTd extraStyles=""><Tooltip message={getSubscriptionTooltipMessage(props.item.pricing)}></Tooltip></UnderlinedTd>
+                                </tr>
+                                <tr>
+                                    <UnderlinedTd extraStyles="text-slate-400 dark:text-slate-200 text-sm" >Debit Times:</UnderlinedTd>
+                                    <UnderlinedTd extraStyles="font-semibold">{props.item.debitTimes} payment{props.item.debitTimes === 1 ? "" : "s"}</UnderlinedTd>
+                                    <UnderlinedTd extraStyles=""><Tooltip message="The amount of times this approval lets the payee debit the account"></Tooltip></UnderlinedTd>
+                                </tr>
+                                <tr>
+                                    <UnderlinedTd extraStyles="text-slate-400 dark:text-slate-200 text-sm">Debit Interval (Days):</UnderlinedTd>
+                                    <UnderlinedTd extraStyles="font-semibold">{getDebitIntervalText(props.item.debitInterval, props.item.debitTimes)}</UnderlinedTd>
+                                    <UnderlinedTd extraStyles=""><Tooltip message="The amount of days that needs to pass before the account can be debited again, counted from the last payment date"></Tooltip></UnderlinedTd>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
                 <div class="w-full">
@@ -82,4 +111,32 @@ export default function BuyPageLayout(props: BuyPagelayoutProps) {
             </div>
         </div>
     </>
+}
+
+function getSubscriptionTooltipMessage(pricing: string) {
+    if (pricing === "Fixed") {
+        return "Fixed priced subscriptions will always debit the approved amount."
+    } else {
+        return "Metered subscriptions have dynamic pricing where the approved amount represents a maximum";
+    }
+}
+
+interface TooltipProps {
+    message: string
+}
+
+function Tooltip(props: TooltipProps) {
+    return <div class="tooltip">
+        ?
+        <span aria-label="Tooltip helptext" class="tooltiptext">{props.message}</span>
+    </div>
+}
+
+interface UnderlinedTdProps {
+    children: ComponentChildren
+    extraStyles: string
+}
+
+function UnderlinedTd(props: UnderlinedTdProps) {
+    return <td class={`${props.extraStyles} border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400`}>{props.children}</td>
 }
