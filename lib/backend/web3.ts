9a1381610597226e1505b5e26c88de8d0e7ea423
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-02 21:11:36 +0200

    feat: added contract calls and estimateGas

--- lib/backend/web3.ts
+++ lib/backend/web3.ts
@@ -7,6 +7,7 @@ import {
   getDirectDebitContractAddress,
   rpcUrl,
 } from "../shared/web3.ts";
+import { Buffer } from "https://deno.land/x/node_buffer@1.1.0/mod.ts";
 
 export function validateAddress(address: string) {
   return ethers.isAddress(address);
@@ -20,6 +21,7 @@ export function getProvider(networkId: string) {
 // I need to implement the server side Contract functions here with ethers js
 export function getContract(provider: any, networkId: string) {
   const address = getDirectDebitContractAddress[networkId as ChainIds];
+
   return new ethers.Contract(
     address,
     DirectDebitArtifact.abi,
@@ -32,4 +34,67 @@ export async function getAccount(commitment: string, networkId: string) {
   const contract = getContract(provider, networkId);
   const account = await contract.accounts(commitment);
   return { account, exists: account.creator !== ZeroAddress };
+}
+
+export async function getEncryptedNote(commitment: string, networkId: string) {
+  const provider = getProvider(networkId);
+  const contract = getContract(provider, networkId);
+  const encryptedNote = await contract.encryptedNotes(commitment);
+  return encryptedNote;
+}
+
+interface DirectDebitArgs {
+  proof: any;
+  publicSignals: any;
+  payeeAddress: string;
+  maxDebitAmount: string;
+  actualDebitedAmount: string;
+  debitTimes: number;
+  debitInterval: number;
+}
+
+export async function estimateRelayerGas(
+  args: DirectDebitArgs,
+  networkId: string,
+) {
+  const provider = getProvider(networkId);
+  const contract = getContract(provider, networkId);
+  const publicSignals = JSON.parse(args.publicSignals);
+
+  return await contract.directdebit.estimateGas(
+    packToSolidityProof(JSON.parse(args.proof)),
+    [
+      toNoteHex(publicSignals[0]),
+      toNoteHex(publicSignals[1]),
+    ],
+    args.payeeAddress,
+    [
+      ethers.parseEther(args.maxDebitAmount),
+      args.debitTimes,
+      args.debitInterval,
+      ethers.parseEther(args.actualDebitedAmount),
+    ],
+  );
+}
+
+function packToSolidityProof(proof: any) {
+  return [
+    proof.pi_a[0],
+    proof.pi_a[1],
+    proof.pi_b[0][1],
+    proof.pi_b[0][0],
+    proof.pi_b[1][1],
+    proof.pi_b[1][0],
+    proof.pi_c[0],
+    proof.pi_c[1],
+  ];
+}
+
+/** BigNumber to hex string of specified length */
+function toNoteHex(number: any, length = 32) {
+  const str = number instanceof Buffer
+    //@ts-ignore buffer does have that hex arg
+    ? number.toString("hex")
+    : BigInt(number).toString(16);
+  return "0x" + str.padStart(length * 2, "0");
 }
