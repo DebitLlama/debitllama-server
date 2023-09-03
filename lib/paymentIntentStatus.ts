2ad52caab75b67f1f243ec94641b08c1159dd2fe
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-03 23:02:38 +0200

    updated payment intent status

--- lib/paymentIntentStatus.ts
+++ lib/paymentIntentStatus.ts
@@ -1,6 +1,6 @@
 export enum PaymentIntentStatus {
   CREATED = "Created",
   CANCELLED = "Cancelled",
-  EXPIRED = "Expired",
-  INPROGRESS = "In Progress",
+  RECURRING = "Recurring",
+  PAID = "Paid",
 }
