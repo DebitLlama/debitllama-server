b93dec19c018e0410dcc0d6953ce80ec24da97b2
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-03 23:00:41 +0200

    created account card element

---
+++ components/AccountCardElement.tsx
@@ -0,0 +1,43 @@
+
+export interface AccountCardElementProps {
+    network: string,
+    balance: string,
+    currency: string,
+    name: string
+}
+
+export function AccountCardElement(props: AccountCardElementProps) {
+    return <div class="accountCard_container">
+        <div class="account_card">
+            <div class="visa_logo">
+                <img src="/logo_white.svg" alt="" />
+            </div>
+            <div class="visa_info">
+                <img src="/chip.png" alt="" />
+                <p>{props.name}</p>
+            </div>
+            <div class="visa_crinfo">
+                <p>{props.balance}{" "}{props.currency} </p>
+                <p>{props.network}</p>
+            </div>
+        </div>
+    </div>
+}
+
+//TODO: create one for checkout also!
+
+export function CheckoutAccountCardElement(props: AccountCardElementProps) {
+    return <div class="checkout_accountCard_container">
+        <div class="checkout_account_card">
+            {/* <div class="checkout_visa_logo">
+                <img src="/logo_white.svg" alt="" />
+            </div> */}
+            <div class="checkout_visa_info">
+                <p>{props.name}</p>
+            </div>
+            <div class="checkout_visa_crinfo">
+                <p>{props.balance}{" "}{props.currency} </p>
+            </div>
+        </div>
+    </div>
+}
