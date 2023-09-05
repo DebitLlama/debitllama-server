4a2f3ae3ee6733f690edf3dabfd7155f8e111787
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-05 19:30:12 +0200

    added identifier to table

--- islands/DebitItemIsland.tsx
+++ islands/DebitItemIsland.tsx
@@ -1,6 +1,4 @@
-import { ComponentChildren } from "preact";
-import { useState } from 'preact/hooks';
-import { PaymentIntentsTableProps, getNextPaymentDateDisplay, getStatusLogo } from "../components/PaymentIntentsTable.tsx";
+import { PaymentIntentsTableProps, RenderIdentifier, getNextPaymentDateDisplay, getStatusLogo } from "../components/PaymentIntentsTable.tsx";
 
 interface DebitItemIsland {
     paymentIntentData: Array<any>
@@ -16,14 +14,16 @@ export function PaymentIntentsTableForDebitItem(props: PaymentIntentsTableProps)
         console.log("click")
     }
 
-    return <><div class="flex flex-col w-full">
+    return <><div class="flex flex-col w-full p-2">
         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                 <div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
                     <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                         <thead class="bg-gray-50 dark:bg-gray-800">
                             <tr>
-
+                                <th scope="col" class="w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    Identifier
+                                </th>
                                 <th scope="col" class="w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                     Status
                                 </th>
@@ -46,6 +46,9 @@ export function PaymentIntentsTableForDebitItem(props: PaymentIntentsTableProps)
                                 const currency = JSON.parse(data.currency);
                                 const currencyName = currency.name;
                                 return <tr class="cursor-pointer bg-white hover:bg-gray-300" onClick={paymentIntentRowClicked}>
+                                    <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                                        {RenderIdentifier(data.paymentIntent)}
+                                    </td>
                                     <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
                                         {getStatusLogo(data.statusText)}
                                     </td>
