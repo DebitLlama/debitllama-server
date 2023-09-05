ced3bb422c1582eb3565e31c70713c54136fae86
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-05 23:01:12 +0200

    added topup history table

--- routes/app/relayer.tsx
+++ routes/app/relayer.tsx
@@ -3,25 +3,23 @@ import { State } from "../_middleware.ts";
 import { Handlers, PageProps } from "$fresh/server.ts";
 import RelayerDetailsCard from "../../islands/RelayerDetailsCard.tsx";
 import { fetchTopUpEvent, getRelayerTopUpContract } from "../../lib/backend/web3.ts";
-import { ChainIds, mapNetworkNameToDBColumnNameString, networkNameFromId } from "../../lib/shared/web3.ts";
+import { insertNewRelayerBalance, selectProfileByUserId, selectRelayerBalanceByUserId, selectRelayerTopUpHistoryDataByTransactionHash, selectRelayerTopUpHistoryDataByUserId, updateRelayerBalanceAndHistorySwitchNetwork } from "../../lib/backend/supabaseQueries.ts";
+import RelayerTopupHistory from "../../islands/RelayerTopupHistory.tsx";
 
 export const handler: Handlers<any, State> = {
     async GET(_req, ctx) {
         const userid = ctx.state.userid;
 
-        const { data: relayerBalanceData, error: relayerBalanceDataError } = await ctx.state.supabaseClient.from("RelayerBalance").select().eq("user_id", userid);
+        const { data: relayerBalanceData, error: relayerBalanceDataError } = await selectRelayerBalanceByUserId(ctx.state.supabaseClient, userid);
 
         if (relayerBalanceData === null || relayerBalanceData.length === 0) {
             // If it doesn't exist I create a new one!
-            const res = await ctx.state.supabaseClient.from("RelayerBalance").insert({
-                created_at: new Date().toUTCString(),
-                user_id: userid,
-            })
-            const { data: relayerBalanceData, error: relayerBalanceDataError } = await ctx.state.supabaseClient.from("RelayerBalance").select().eq("user_id", userid);
+            const res = await insertNewRelayerBalance(ctx.state.supabaseClient, userid)
+            const { data: relayerBalanceData, error: relayerBalanceDataError } = await selectRelayerBalanceByUserId(ctx.state.supabaseClient, userid);
             return ctx.render({ ...ctx.state, relayerBalanceData })
         }
 
-        const { data: profileData, error: profileError } = await ctx.state.supabaseClient.from("Profiles").select().eq("userid", userid);
+        const { data: profileData, error: profileError } = await selectProfileByUserId(ctx.state.supabaseClient, userid);
 
         if (profileData === null || profileData.length === 0) {
             // Redirect to the profile page instead so the user needs to fill it out!
@@ -30,7 +28,9 @@ export const handler: Handlers<any, State> = {
             return new Response(null, { status: 303, headers })
         }
 
-        return ctx.render({ ...ctx.state, relayerBalanceData, profileData })
+        const { data: relayerTopUpHistoryData, error: relayerTopUpHistoryDataError } = await selectRelayerTopUpHistoryDataByUserId(ctx.state.supabaseClient, userid);
+
+        return ctx.render({ ...ctx.state, relayerBalanceData, profileData, relayerTopUpHistoryData });
     },
     async POST(_req, ctx) {
         const userid = ctx.state.userid;
@@ -40,11 +40,9 @@ export const handler: Handlers<any, State> = {
         const transactionHash = json.transactionHash;
         const from = json.from;
         const amount = json.amount;
-        console.log("json")
-        console.log(json)
         // I need to decode the json sent to the server, 
 
-        const { data: profileData, error: profileError } = await ctx.state.supabaseClient.from("Profiles").select().eq("userid", userid);
+        const { data: profileData, error: profileError } = await selectProfileByUserId(ctx.state.supabaseClient, userid);
         if (profileData === null || profileData.length === 0) {
             return new Response("Invalid user Profile!", { status: 500 })
         }
@@ -54,10 +52,8 @@ export const handler: Handlers<any, State> = {
         }
         //Check if the tx is already saved into supabase I can't add it to relayer balance
 
-        const { data: relayerTopUpHistoryData, error: relayerTopUpHistoryDataError } = await ctx.state.supabaseClient
-            .from("RelayerTopUpHistory")
-            .select()
-            .eq("transactionHash", transactionHash);
+        const { data: relayerTopUpHistoryData, error: relayerTopUpHistoryDataError } = await selectRelayerTopUpHistoryDataByTransactionHash(
+            ctx.state.supabaseClient, transactionHash);
 
         // I need the opposite of this to occur for an error hence the !
         if (!(relayerTopUpHistoryData === null || relayerTopUpHistoryData.length === 0)) {
@@ -72,20 +68,25 @@ export const handler: Handlers<any, State> = {
             return new Response(null, { status: 500 })
         }
 
-        //         const updateBalance = {
-        // [mapNetworkNameToDBColumnNameString[networkNameFromId[chainId as ChainIds]]] : 
-        //         }
+        // Check that the transaction hash from the event matches the transactionHash sent by the API
+        if (topUpEvents[0].transactionHash.toLowerCase() !== transactionHash.toLowerCase()) {
+            return new Response(null, { status: 500 })
+        }
 
-        //         // Now I add the balance to the relayerBalance and save the transaction to history
-        //         await ctx.state.supabaseClient.from("RelayerBalance").update({
+        await updateRelayerBalanceAndHistorySwitchNetwork(
+            chainId,
+            ctx.state.supabaseClient,
+            userid,
+            amount,
+            transactionHash
 
-        //         })
-
-
+        )
 
         return new Response(null, { status: 200 })
     }
 }
+
+
 
 export default function Relayer(props: PageProps) {
     return <Layout isLoggedIn={props.data.token}>
@@ -97,9 +98,16 @@ export default function Relayer(props: PageProps) {
                             <img src="/blockchain.png" width="40px" />
                             <p class={"text-xl font-extrabold leading-10 pl-6"}>Relayer</p>
                         </div>
-                        <RelayerDetailsCard walletAddress={props.data.profileData[0].walletaddress} relayerData={props.data.relayerBalanceData[0]}></RelayerDetailsCard>
+                        <RelayerDetailsCard
+                            walletAddress={props.data.profileData[0].walletaddress}
+                            relayerData={props.data.relayerBalanceData[0]}></RelayerDetailsCard>
                     </div>
                 </div>
+            </div>
+
+            <div class="flex flex-col items-center justify-center h-full w-full">
+                <h4>Top Up History</h4>
+                <RelayerTopupHistory topUpHistoryData={props.data.relayerTopUpHistoryData}></RelayerTopupHistory>
             </div>
         </div>
     </Layout>
