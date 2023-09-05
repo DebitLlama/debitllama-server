7a340ac063e9b77305d62bae833cc54474f33b57
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-05 19:32:25 +0200

    added new functions

--- lib/shared/web3.ts
+++ lib/shared/web3.ts
@@ -29,6 +29,10 @@ export enum DonauTestnetTokens {
   USDTM = "0x4420a4415033bd22393d3A918EF8d2c9c62efD99",
 }
 
+export enum RelayerGasTrackerContractAddress {
+  BTT_TESTNET = "0x3e4E07926c1c4AC9f29539E385fBbF700b49F221",
+}
+
 export const rpcUrl: { [key in ChainIds]: RPCURLS } = {
   [ChainIds.BTT_TESTNET_ID]: RPCURLS.BTT_TESTNET,
 };
@@ -47,6 +51,12 @@ export const getDirectDebitContractAddress: {
   [ChainIds.BTT_TESTNET_ID]: DirectDebitContractAddress.BTT_TESTNET,
 };
 
+export const getRelayerGasTrackerContractAddress: {
+  [keys in ChainIds]: RelayerGasTrackerContractAddress;
+} = {
+  [ChainIds.BTT_TESTNET_ID]: RelayerGasTrackerContractAddress.BTT_TESTNET,
+};
+
 export const networkNameFromId: { [key in ChainIds]: NetworkNames } = {
   [ChainIds.BTT_TESTNET_ID]: NetworkNames.BTT_TESTNET,
 };
@@ -54,3 +64,28 @@ export const networkNameFromId: { [key in ChainIds]: NetworkNames } = {
 export const chainIdFromNetworkName: { [key in NetworkNames]: ChainIds } = {
   [NetworkNames.BTT_TESTNET]: ChainIds.BTT_TESTNET_ID,
 };
+
+//This is used with the RelayerBalance DB Table!
+export function mapNetworkNameToDBColumn(
+  selectedNetwork: NetworkNames,
+  relayerData: any,
+) {
+  switch (selectedNetwork) {
+    case NetworkNames.BTT_TESTNET:
+      return relayerData.BTT_Donau_Testnet_Balance;
+    default:
+      return "Invalid Network";
+  }
+}
+
+//This is used with the RelayerBalance DB Table!
+export function mapNetworkNameToDBColumnNameString(
+  selectedNetwork: NetworkNames,
+) {
+  switch (selectedNetwork) {
+    case NetworkNames.BTT_TESTNET:
+      return "BTT_Donau_Testnet_Balance";
+    default:
+      return "";
+  }
+}
