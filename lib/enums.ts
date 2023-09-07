4b7a756a9778d716636af9a6b1ad0fe39747de75
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-07 06:51:53 +0200

    created types for paymentIntent row

--- lib/enums.ts
+++ lib/enums.ts
@@ -4,9 +4,65 @@ export enum PaymentIntentStatus {
   RECURRING = "Recurring",
   PAID = "Paid",
   BALANCETOOLOWTORELAY = "Balance too low to relay",
+  ACCOUNTBALANCETOOLOW = "Account Balance too low",
 }
 
 export enum Pricing {
   Fixed = "Fixed",
   Dynamic = "Dynamic",
 }
+
+export type PaymentIntentRow = {
+  id: number;
+  created_at: string;
+  creator_user_id: number;
+  payee_user_id: string;
+  account_id: Account;
+  payee_address: string;
+  maxDebitAmount: string;
+  debitTimes: number;
+  debitInterval: number;
+  paymentIntent: string;
+  commitment: string;
+  estimatedGas: string;
+  statusText: string;
+  lastPaymentDate: string | null;
+  nextPaymentDate: string | null;
+  pricing: string;
+  currency: string;
+  network: string;
+  debit_item_id: DebitItem;
+  used_for: number;
+  proof: string;
+  publicSignals: string;
+};
+
+export type Account = {
+  id: number;
+  created_at: string;
+  user_id: string;
+  network_id: string;
+  commitment: string;
+  name: string;
+  closed: boolean;
+  currency: string;
+  balance: string;
+};
+
+export type DebitItem = {
+  id: number;
+  created_at: string;
+  payee_id: string;
+  payee_address: string;
+  currency: string;
+  max_price: string;
+  debit_times: number;
+  debit_interval: number;
+  button_id: string;
+  redirect_url: string;
+  pricing: string;
+  network: string;
+  name: string;
+  deleted: boolean;
+  payment_intents_count: number;
+};
