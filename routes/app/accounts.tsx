29aa84cd560356453e26d529b5b44980000bb540
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-07 06:50:18 +0200

    added missed payments

--- routes/app/accounts.tsx
+++ routes/app/accounts.tsx
@@ -1,21 +1,21 @@
 import Layout from "../../components/Layout.tsx";
 import { State } from "../_middleware.ts";
 import { Handlers, PageProps } from "$fresh/server.ts";
-import { AccountCardElement } from "../../components/AccountCardElement.tsx";
 import AccountCardCarousel from "../../islands/accountCardCarousel.tsx";
-import { selectOpenAccountsByIdDESC, selectPaymentIntentsByUserIdDESC } from "../../lib/backend/supabaseQueries.ts";
+import { selectOpenAccountsByIdDESC, selectPaymentIntentsByAccountBalanceTooLow, selectPaymentIntentsByUserIdDESC } from "../../lib/backend/supabaseQueries.ts";
 
 export const handler: Handlers<any, State> = {
     async GET(_req, ctx) {
         const { data: accountsData, error: accountsError } = await selectOpenAccountsByIdDESC(ctx.state.supabaseClient, ctx.state.userid);
-        ;
 
         // I also need to fetch the payment intents here!
 
         const { data: paymentIntentData, error: paymentIntentError } = await selectPaymentIntentsByUserIdDESC(ctx.state.supabaseClient, ctx.state.userid);
 
+        const { data: missedPayments, error: missedPaymentsError } = await selectPaymentIntentsByAccountBalanceTooLow(ctx.state.supabaseClient, ctx.state.userid);
+
         // Add pagination for both!
-        return ctx.render({ ...ctx.state, accountsData, paymentIntentData })
+        return ctx.render({ ...ctx.state, accountsData, paymentIntentData, missedPayments })
     }
 }
 
@@ -25,7 +25,10 @@ export default function Accounts(props: PageProps) {
             <section class="flex flex-row">
                 <a href={"/app/addNewAccount"} class="mb-8 bg-gradient-to-b w-max mx-auto text-indigo-500 font-semibold from-slate-50 to-indigo-100 px-10 py-3 rounded-2xl shadow-indigo-400 shadow-md border-b-4 hover border-b border-indigo-200 hover:shadow-sm transition-all duration-500">Create New Account</a>
             </section>
-            <AccountCardCarousel paymentIntents={props.data.paymentIntentData} accountData={props.data.accountsData}></AccountCardCarousel>
+            <AccountCardCarousel
+                missedPayments={props.data.missedPayments}
+                paymentIntents={props.data.paymentIntentData}
+                accountData={props.data.accountsData}></AccountCardCarousel>
         </Layout>
     );
 }
