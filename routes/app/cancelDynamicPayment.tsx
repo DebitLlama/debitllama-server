0bac4c27fe1512c501b8b6743ecf137fc9c6244d
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-08 21:15:14 +0200

    feat: refund gas allocation when cancelling

--- routes/app/cancelDynamicPayment.tsx
+++ routes/app/cancelDynamicPayment.tsx
@@ -1,6 +1,8 @@
 import { Handlers } from "$fresh/server.ts";
 import { errorResponseBuilder, successResponseBuilder } from "../../lib/backend/responseBuilders.ts";
-import { deleteDynamicPaymentRequestJobById } from "../../lib/backend/supabaseQueries.ts";
+import { deleteDynamicPaymentRequestJobById, selectDynamicPaymentRequestJobById, updateRelayerBalanceWithAllocatedAmount } from "../../lib/backend/supabaseQueries.ts";
+import { getRelayerBalanceForChainId } from "../../lib/backend/web3.ts";
+import { ChainIds } from "../../lib/shared/web3.ts";
 import { State } from "../_middleware.ts";
 
 
@@ -9,12 +11,27 @@ export const handler: Handlers<any, State> = {
         const json = await _req.json();
         const dynamicPaymentRequestId = json.dynamicPaymentRequestId;
 
+        const { data: selectedDynamicPaymentRequest, error: selectedDynamicErr } = await selectDynamicPaymentRequestJobById(
+            ctx.state.supabaseClient, dynamicPaymentRequestId)
+
         // this will delete the row by id if it was created by the user and the row is not locked!
         const result = await deleteDynamicPaymentRequestJobById(
             ctx.state.supabaseClient,
             dynamicPaymentRequestId,
             ctx.state.userid
         )
+        const chainId = selectedDynamicPaymentRequest[0].paymentIntent_id.network as ChainIds;
+        const relayerBalance = selectedDynamicPaymentRequest[0].relayerBalance_id;
+        const allocatedGas = selectedDynamicPaymentRequest[0].allocatedGas;
+        const res = await updateRelayerBalanceWithAllocatedAmount(
+            ctx.state.supabaseClient,
+            relayerBalance.id,
+            chainId,
+            getRelayerBalanceForChainId(chainId, relayerBalance),
+            allocatedGas,
+            "0"
+        )
+
         if (result.error !== null) {
             return errorResponseBuilder("Unable to delete Payment Request!")
         } else {
