d58efdd185efc3da6e4164b032b78d56d9e484a5
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-10 02:36:35 +0200

    addNewAccount button is now a FAB

--- routes/app/accounts.tsx
+++ routes/app/accounts.tsx
@@ -22,13 +22,13 @@ export const handler: Handlers<any, State> = {
 export default function Accounts(props: PageProps) {
     return (
         <Layout isLoggedIn={props.data.token}>
-            <section class="flex flex-row">
-                <a href={"/app/addNewAccount"} class="mb-8 bg-gradient-to-b w-max mx-auto text-indigo-500 font-semibold from-slate-50 to-indigo-100 px-10 py-3 rounded-2xl shadow-indigo-400 shadow-md border-b-4 hover border-b border-indigo-200 hover:shadow-sm transition-all duration-500">Create New Account</a>
-            </section>
+            <a href={"/app/addNewAccount"} class="fixed z-90 bottom-10 right-8 bg-gray-100	text-indigo-500 font-semibold from-slate-50 to-indigo-100 px-10 py-3 rounded-2xl shadow-indigo-400 shadow-md border-b-4 hover border-b border-indigo-200 hover:shadow-sm transition-all duration-500">Create New Account</a>
             <AccountCardCarousel
                 missedPayments={props.data.missedPayments}
                 paymentIntents={props.data.paymentIntentData}
                 accountData={props.data.accountsData}></AccountCardCarousel>
+            <hr class="w-48 h-1 mx-auto my-8 border-0 rounded md:my-10 " />
+
         </Layout>
     );
 }
