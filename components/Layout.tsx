83b343297f80ab83941104534b9c7f9b680247b2
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-10 02:23:16 +0200

    moved footer to the bottom

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -18,13 +18,13 @@ export default function Layout(props: LayoutProps) {
         <script src="/directdebit_bundle.js"></script>
       </Head>
       <Nav loggedIn={props.isLoggedIn} />
-      <div class="p-2 mx-auto">
+      <div class="p-2 mx-auto" style="min-height: 88vh;">
         {props.children}
       </div>
       <footer
         class="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
         <div class="p-4 text-center text-neutral-700 dark:text-neutral-200 flex flex-col">
-          © ZKP Tech Solutions Ltd 2023 Copyright {" "}
+          © ZKP Tech Solutions Ltd 2023{" "}
           <a
             class="text-gray-500 hover:text-gray-700 py-1 border-gray-500"
             href="/app/feedback"
