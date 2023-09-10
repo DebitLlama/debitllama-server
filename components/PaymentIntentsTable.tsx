bb50a08b7a8ef697ff830fc664636ff5e94dc76d
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-10 08:54:23 +0200

    updated table

--- components/PaymentIntentsTable.tsx
+++ components/PaymentIntentsTable.tsx
@@ -1,8 +1,15 @@
-import { Pricing } from "../lib/enums.ts";
+import { PaymentIntentsTableColNames, Pricing } from "../lib/enums.ts";
 import { RenderIdentifier, getPaymentIntentStatusLogo } from "./components.tsx";
 
 export interface PaymentIntentsTableProps {
     paymentIntentData: Array<any>
+}
+
+export interface PaymentIntentsTablePropWithFilter {
+    paymentIntentData: Array<any>
+    headerClicked: (at: PaymentIntentsTableColNames) => () => void;
+    sortBy: PaymentIntentsTableColNames,
+    sortDirection: "ASC" | "DESC"
 }
 
 export function getNextPaymentDateDisplay(nextPaymentDate: any) {
@@ -17,7 +24,7 @@ function getPaymentColValue(pricing: string, maxDebitAmount: string, currencyNam
     }
 }
 
-export function PaymentIntentsTableForAccounts(props: PaymentIntentsTableProps) {
+export function PaymentIntentsTableForAccounts(props: PaymentIntentsTablePropWithFilter) {
 
     function paymentIntentRowClicked(paymentIntent: string) {
         return () => location.href = `/app/createdPaymentIntents?q=${paymentIntent}`
@@ -28,26 +35,31 @@ export function PaymentIntentsTableForAccounts(props: PaymentIntentsTableProps) 
             <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                 <div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
                     <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                        <thead class="bg-gray-50 dark:bg-gray-800">
+                        <thead class="bg-gray-50 dark:bg-gray-800 select-none">
                             <tr>
-                                <th scope="col" class="w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                    Identifier
+                                <th tabIndex={1} onClick={props.headerClicked(PaymentIntentsTableColNames.Identifier)} scope="col" class="cursor-pointer w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400 hover:bg-gray-200">
+                                    <div class="flex flex-row"> Identifier {getArrows(props.sortDirection, PaymentIntentsTableColNames.Identifier, props.sortBy)}</div>
                                 </th>
-                                <th scope="col" class="w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                    Status
+                                <th tabIndex={2} onClick={props.headerClicked(PaymentIntentsTableColNames.Status)} scope="col" class="cursor-pointer w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400 hover:bg-gray-200">
+                                    <div class="flex flex-row"> Status {getArrows(props.sortDirection, PaymentIntentsTableColNames.Status, props.sortBy)}</div>
                                 </th>
 
-                                <th scope="col" class="w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                    Payment
+                                <th tabIndex={3} onClick={props.headerClicked(PaymentIntentsTableColNames.Payment)} scope="col" class="cursor-pointer w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400 hover:bg-gray-200">
+                                    <div class="flex flex-row">  Amount {getArrows(props.sortDirection, PaymentIntentsTableColNames.Payment, props.sortBy)}</div>
                                 </th>
-                                <th scope="col" class="w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                    Payments left
+
+                                <th tabIndex={4} onClick={props.headerClicked(PaymentIntentsTableColNames.DebitTimes)} scope="col" class="cursor-pointer w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400 hover:bg-gray-200">
+                                    <div class="flex flex-row"> Total Payments {getArrows(props.sortDirection, PaymentIntentsTableColNames.DebitTimes, props.sortBy)}</div>
                                 </th>
-                                <th scope="col" class="w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                    Next payment
+
+                                <th tabIndex={5} onClick={props.headerClicked(PaymentIntentsTableColNames.UsedFor)} scope="col" class="cursor-pointer w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400 hover:bg-gray-200">
+                                    <div class="flex flex-row"> Past Payments {getArrows(props.sortDirection, PaymentIntentsTableColNames.UsedFor, props.sortBy)}</div>
                                 </th>
-                                <th scope="col" class="w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                    Created Date
+                                <th tabIndex={6} onClick={props.headerClicked(PaymentIntentsTableColNames.NextPayment)} scope="col" class="cursor-pointer w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400 hover:bg-gray-200">
+                                    <div class="flex flex-row"> Next payment {getArrows(props.sortDirection, PaymentIntentsTableColNames.NextPayment, props.sortBy)}</div>
+                                </th>
+                                <th tabIndex={7} onClick={props.headerClicked(PaymentIntentsTableColNames.CreatedDate)} scope="col" class="cursor-pointer w-1/6 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400 hover:bg-gray-200">
+                                    <div class="flex flex-row">Created Date {getArrows(props.sortDirection, PaymentIntentsTableColNames.CreatedDate, props.sortBy)}</div>
                                 </th>
                             </tr>
                         </thead>
@@ -55,12 +67,12 @@ export function PaymentIntentsTableForAccounts(props: PaymentIntentsTableProps) 
                             {props.paymentIntentData.map((data) => {
                                 const currency = JSON.parse(data.currency);
                                 const currencyName = currency.name;
-                                return <tr class="cursor-pointer bg-white hover:bg-gray-300" onClick={paymentIntentRowClicked(data.paymentIntent)}>
+                                return <tr tabIndex={props.paymentIntentData.indexOf(data) + 7} class="cursor-pointer bg-white hover:bg-gray-300" onClick={paymentIntentRowClicked(data.paymentIntent)}>
                                     <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
                                         {RenderIdentifier(data.paymentIntent)}
                                     </td>
                                     <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                        {getPaymentIntentStatusLogo(data.statusText,"account")}
+                                        {getPaymentIntentStatusLogo(data.statusText, "account")}
                                     </td>
                                     <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
                                         <div class="flex items-center gap-x-2">
@@ -69,7 +81,8 @@ export function PaymentIntentsTableForAccounts(props: PaymentIntentsTableProps) 
                                             </div>
                                         </div>
                                     </td>
-                                    <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">{data.debitTimes - data.used_for}</td>
+                                    <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">{data.debitTimes}</td>
+                                    <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">{data.used_for}</td>
                                     <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">{getNextPaymentDateDisplay(data.nextPaymentDate)}</td>
                                     <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
                                         {new Date(data.created_at).toLocaleString()}
@@ -87,3 +100,13 @@ export function PaymentIntentsTableForAccounts(props: PaymentIntentsTableProps) 
 }
 
 
+function getArrows(direction: "ASC" | "DESC", sortBy: PaymentIntentsTableColNames, currentSortBy: PaymentIntentsTableColNames) {
+    if (sortBy !== currentSortBy) {
+        return ""
+    }
+    if (direction === "ASC") {
+        return <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path style="fill: #6b7280;" d="M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z" /></svg>
+    } else {
+        return <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path style="fill: #6b7280;" d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" /></svg>
+    }
+}
