20949474d6aa49310e1d5856eb30aa77bbb2c019
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-10 02:33:01 +0200

    updated nav styles

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -17,21 +17,21 @@ export default function Nav({ loggedIn }: NavProps) {
     ]
 
     return (
-        <div class="bg-white max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div class="sticky top-0 z-50 bg-gray-50 flex flex-wrap items-center justify-between mx-auto px-4 pt-4 pb-2 shadow-md	">
             <div class="text-2xl  ml-1 font-bold flex flex-row">
                 <img src="/logo.svg" width="45" class={"mr-3"} />{" "}
                 <span class="mt-1">Debit</span><span class="text-gray-600 mt-1">Llama</span>
             </div>
-            <ul class="flex gap-6">
+            <ul class="flex gap-4 mb-1 mt-2">
                 {
                     loggedIn ? (
                         loggedInMenus.map((menu) => (
                             <li>
                                 <a
                                     href={menu.href}
-                                    class="text-lg text-gray-500 hover:text-gray-700 py-1 border-gray-500"
+                                    class="rounded-md bg-gray-200 hover:bg-gray-400 text-lg text-gray-600 hover:text-gray-950 border-gray-500 p-2"
                                 >
-                                    {menu.name}
+                                    {renderMenuButton(menu.name)}
                                 </a>
                             </li>
                         ))
@@ -40,7 +40,7 @@ export default function Nav({ loggedIn }: NavProps) {
                             <li>
                                 <a
                                     href={menu.href}
-                                    class="text-lg text-gray-500 hover:text-gray-700 py-1 border-gray-500"
+                                    class="rounded-md bg-gray-200 hover:bg-gray-400 text-lg text-gray-600 hover:text-gray-950 border-gray-500 p-2"
                                 >
                                     {menu.name}
                                 </a>
@@ -51,4 +51,27 @@ export default function Nav({ loggedIn }: NavProps) {
             </ul>
         </div>
     );
+}
+
+function renderMenuButton(menuName: string) {
+    switch (menuName) {
+        case "Accounts":
+            return menuName;
+        case "Debit":
+            return menuName
+        case "Profile":
+            return menuName
+        case "Logout":
+            return menuName
+        case "Login":
+            return menuName
+        case "SignUp":
+            return menuName
+        default:
+            return menuName
+    }
+}
+
+function getLogoutLogo() {
+    return <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" /></svg>;
 }
