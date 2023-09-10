8d9c9dde3b114e236a7179103a13bc0af569a4ac
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-10 08:57:14 +0200

    moved the add account button to bottom

--- routes/app/accounts.tsx
+++ routes/app/accounts.tsx
@@ -10,25 +10,23 @@ export const handler: Handlers<any, State> = {
         const select = queryBuilder.select();
         const { data: accountsData } = await select.Accounts.whereOpenByUserIdOrderDesc();
 
-        const { data: paymentIntentData } = await select.PaymentIntents.byUserIdForCreatorDesc();
-
         const { data: missedPayments } = await select.PaymentIntents.byAccountBalanceTooLowByUserIdForCreatorDesc();
 
         // Add pagination for both!
-        return ctx.render({ ...ctx.state, accountsData, paymentIntentData, missedPayments })
+        return ctx.render({ ...ctx.state, accountsData, missedPayments })
     }
 }
 
 export default function Accounts(props: PageProps) {
     return (
         <Layout isLoggedIn={props.data.token}>
-            <a href={"/app/addNewAccount"} class="fixed z-90 bottom-10 right-8 bg-gray-100	text-indigo-500 font-semibold from-slate-50 to-indigo-100 px-10 py-3 rounded-2xl shadow-indigo-400 shadow-md border-b-4 hover border-b border-indigo-200 hover:shadow-sm transition-all duration-500">Create New Account</a>
             <AccountCardCarousel
                 missedPayments={props.data.missedPayments}
-                paymentIntents={props.data.paymentIntentData}
                 accountData={props.data.accountsData}></AccountCardCarousel>
             <hr class="w-48 h-1 mx-auto my-8 border-0 rounded md:my-10 " />
-
+            <section class="flex flex-row">
+                <a href={"/app/addNewAccount"} class="mb-8 bg-gradient-to-b w-max mx-auto text-indigo-500 font-semibold from-slate-50 to-indigo-100 px-10 py-3 rounded-2xl shadow-indigo-400 shadow-md border-b-4 hover border-b border-indigo-200 hover:shadow-sm transition-all duration-500">Create New Account</a>
+            </section>
         </Layout>
     );
 }
