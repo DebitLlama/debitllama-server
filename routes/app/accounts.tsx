582ac40bb51983f65d6017dcb9612431a1461d46
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-11 05:25:49 +0200

    updated accounts button location again

--- routes/app/accounts.tsx
+++ routes/app/accounts.tsx
@@ -20,13 +20,14 @@ export const handler: Handlers<any, State> = {
 export default function Accounts(props: PageProps) {
     return (
         <Layout isLoggedIn={props.data.token}>
+            <section class="flex flex-row">
+                <a href={"/app/addNewAccount"} class="mb-8 bg-gradient-to-b w-max mx-auto text-indigo-500 font-semibold from-slate-50 to-indigo-100 px-10 py-3 rounded-2xl shadow-indigo-400 shadow-md border-b-4 hover border-b border-indigo-200 hover:shadow-sm transition-all duration-500">Create New Account</a>
+            </section>
             <AccountCardCarousel
                 missedPayments={props.data.missedPayments}
                 accountData={props.data.accountsData}></AccountCardCarousel>
             <hr class="w-48 h-1 mx-auto my-8 border-0 rounded md:my-10 " />
-            <section class="flex flex-row">
-                <a href={"/app/addNewAccount"} class="mb-8 bg-gradient-to-b w-max mx-auto text-indigo-500 font-semibold from-slate-50 to-indigo-100 px-10 py-3 rounded-2xl shadow-indigo-400 shadow-md border-b-4 hover border-b border-indigo-200 hover:shadow-sm transition-all duration-500">Create New Account</a>
-            </section>
+
         </Layout>
     );
 }
