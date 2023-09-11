b34f29166c5847fad0fcfbb9e367370714d5b165
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-11 11:33:26 +0200

    added white logo to copy button

--- islands/copyButton.tsx
+++ islands/copyButton.tsx
@@ -1,10 +1,11 @@
 export interface CopyButtonProps {
-    str: string
+    str: string;
+    iconColor: "black" | "white"
 }
 
 export default function CopyButton(props: CopyButtonProps) {
 
-    return <button onClick={copyStringToClipboard(props.str)}><img width="20px" src="/copyIcon.svg" /></button>
+    return <button onClick={copyStringToClipboard(props.str)}><img width="20px" src={props.iconColor === "black" ? "/copyIcon.svg" : "/copyIcon_white.svg"} /></button>
 }
 function copyStringToClipboard(str: string) {
     return async () => await navigator.clipboard.writeText(str);
