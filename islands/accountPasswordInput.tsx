25baa564c8e3beae30f935ab5f53f30129e6e620
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-15 10:59:48 +0200

    refactored helper text

--- islands/accountPasswordInput.tsx
+++ islands/accountPasswordInput.tsx
@@ -15,12 +15,8 @@ export default function AccountPasswordInput(props: AccountPasswordInputProps) {
         <label class="block text-gray-700 text-sm font-bold mb-2" for="encryptionpass">Virtual Account Password</label>
         <input value={props.password} onKeyUp={(event: any) => props.setPassword(event.target.value)} onChange={(event: any) => props.setPassword(event.target.value)} required data-lpignore="true" autocomplete={"off"} class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
             type="password" id="encryptionpass" name="encryptionpass" placeholder="********" />
-
-
         <PasswordStrengthDisplay passwordStrengthNotification={props.passwordStrengthNotification}></PasswordStrengthDisplay>
         <p class="text-sm text-gray-500">You need to use a good or strong password</p>
-
-
     </div>
 
         <div class="mb-4">
@@ -29,10 +25,7 @@ export default function AccountPasswordInput(props: AccountPasswordInputProps) {
                 type="password" id="confirm-password" name="confirmpassword" placeholder="********" />
             <p class="text-red-800">{props.passwordMatchError}</p>
         </div>
-        <div class="mb-4">
-            <p class="text-sm ...">The password is used for encrypting your account and it's needed for spending. Do not reuse your login password. The accounts are non-custodial and if you loose your password we can't recover it for you. You can always close the account and withdraw the deposit with the wallet you created it with.</p>
-
-        </div>
+     
     </>
 }
 
