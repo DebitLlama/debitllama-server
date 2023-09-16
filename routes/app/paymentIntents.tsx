132939a06ebc9ca838ad11501d13e35c66b5e64f
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-16 06:02:45 +0200

    created separate payment intents page

---
+++ routes/app/paymentIntents.tsx
@@ -0,0 +1,20 @@
+import Layout from "../../components/Layout.tsx";
+import { State } from "../_middleware.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import PaymentIntentsPaginationForDebitItemsPage from "../../islands/pagination/PaymentIntentsPaginationForDebitItemsPage.tsx";
+
+export const handler: Handlers<any, State> = {
+    GET(_req, ctx) {
+        return ctx.render({ ...ctx.state })
+    }
+}
+
+export default function paymentIntents(props: PageProps) {
+    return <Layout isLoggedIn={props.data.token}>
+        <hr
+            class="my-1 h-3 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
+        <section class="container px-4 mx-auto">
+            <PaymentIntentsPaginationForDebitItemsPage></PaymentIntentsPaginationForDebitItemsPage>
+        </section>
+    </Layout>
+}
