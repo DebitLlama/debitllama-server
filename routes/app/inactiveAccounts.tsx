a3923aa6a9864b493e7fbbc8e340e8778e0efa72
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-16 06:02:55 +0200

    created inactive accounts page

---
+++ routes/app/inactiveAccounts.tsx
@@ -0,0 +1,29 @@
+import Layout from "../../components/Layout.tsx";
+import { State } from "../_middleware.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import AccountCardCarousel from "../../islands/accountCardCarousel.tsx";
+import QueryBuilder from "../../lib/backend/queryBuilder.ts";
+
+export const handler: Handlers<any, State> = {
+    async GET(_req, ctx) {
+        const queryBuilder = new QueryBuilder(ctx);
+        const select = queryBuilder.select();
+        const { data: accountsData } = await select.Accounts.whereClosedByUserIdOrderDesc();
+
+        const { data: missedPayments } = await select.PaymentIntents.byAccountBalanceTooLowByUserIdForCreatorDesc();
+
+        // Add pagination for both!
+        return ctx.render({ ...ctx.state, accountsData, missedPayments })
+    }
+}
+
+export default function InactiveAccounts(props: PageProps) {
+    return (
+        <Layout isLoggedIn={props.data.token}>
+            <AccountCardCarousel
+                missedPayments={props.data.missedPayments}
+                accountData={props.data.accountsData}></AccountCardCarousel>
+            <hr class="w-48 h-1 mx-auto my-8 border-0 rounded md:my-10 " />
+        </Layout>
+    );
+}
