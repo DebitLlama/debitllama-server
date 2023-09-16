080ffc22f87c75b1dc703beca3efd7f30e9a0cf3
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-16 12:25:45 +0200

    updated layout and footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -20,19 +20,20 @@ export default function Layout(props: LayoutProps) {
       </Head>
       <Nav loggedIn={props.isLoggedIn} />
       <SideBar></SideBar>
-      <div class="p-2 mx-auto layoutheight overflow-auto" >
+      <div class="mx-auto layoutheight overflow-auto" id="layout-children">
         {props.children}
+        <footer
+          class="border-grey-200 bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+          <div class="pt-4 text-center text-neutral-700 dark:text-neutral-200 flex flex-col">
+            © ZKP Tech Solutions Ltd 2023{" "}
+            <a
+              class="text-gray-500 hover:text-gray-700 py-1 border-gray-500"
+              href="/app/feedback"
+            >Feedback</a>
+          </div>
+        </footer>
       </div>
-      <footer
-        class="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
-        <div class="pt-4 text-center text-neutral-700 dark:text-neutral-200 flex flex-col">
-          © ZKP Tech Solutions Ltd 2023{" "}
-          <a
-            class="text-gray-500 hover:text-gray-700 py-1 border-gray-500"
-            href="/app/feedback"
-          >Feedback</a>
-        </div>
-      </footer>
+
     </>
   );
 }
