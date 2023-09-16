a42d2b1b63601940583a2c33426280d36372f628
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-16 06:02:17 +0200

    sidebar and nav height adjusted

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -19,35 +19,8 @@ export default function Nav({ loggedIn }: NavProps) {
     ]
 
     return (
-        <div class="sticky top-0 z-50 bg-gray-50 flex flex-wrap items-center justify-between mx-auto px-4 pt-4 pb-2 shadow-md	">
+        <div class="navheight sticky top-0 z-50 bg-gray-50 flex flex-wrap items-center justify-between mx-auto pl-4 pr-4 pt-2 pb-2 shadow-md	">
             <SidebarToggleButton></SidebarToggleButton>
-            {/* <ul class="flex gap-4 mb-1 mt-2">
-                {
-                    loggedIn ? (
-                        loggedInMenus.map((menu) => (
-                            <li>
-                                <a
-                                    href={menu.href}
-                                    class="rounded-md bg-gray-200 hover:bg-gray-400 text-lg text-gray-600 hover:text-gray-950 border-gray-500 p-2"
-                                >
-                                    {renderMenuButton(menu.name)}
-                                </a>
-                            </li>
-                        ))
-                    ) : (
-                        nonLoggedInMenus.map((menu) => (
-                            <li>
-                                <a
-                                    href={menu.href}
-                                    class="rounded-md bg-gray-200 hover:bg-gray-400 text-lg text-gray-600 hover:text-gray-950 border-gray-500 p-2"
-                                >
-                                    {menu.name}
-                                </a>
-                            </li>
-                        ))
-                    )
-                }
-            </ul> */}
             <a href="/app/accounts">
                 <div class="text-2xl  ml-1 font-bold flex flex-row">
                     <img src="/logo.svg" width="45" class={"mr-3"} />{" "}
@@ -58,24 +31,6 @@ export default function Nav({ loggedIn }: NavProps) {
     );
 }
 
-function renderMenuButton(menuName: string) {
-    switch (menuName) {
-        case "Accounts":
-            return menuName;
-        case "Debit":
-            return menuName
-        case "Profile":
-            return menuName
-        case "Logout":
-            return menuName
-        case "Login":
-            return menuName
-        case "SignUp":
-            return menuName
-        default:
-            return menuName
-    }
-}
 
 function getLogoutLogo() {
     return <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" /></svg>;
