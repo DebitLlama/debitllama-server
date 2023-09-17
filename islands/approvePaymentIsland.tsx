98820665f37012a7354eb49c9904a7dcce0ab432
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-17 17:51:42 +0200

    updated UI layout

--- islands/approvePaymentIsland.tsx
+++ islands/approvePaymentIsland.tsx
@@ -80,27 +80,43 @@ export default function ApprovePaymentIsland(props: ApprovePaymentIslandProps) {
         <div class="flex flex-col flex-wrap">
         </div>
         {success ? <SuccessAnimation></SuccessAnimation> : <>
-            <div class="flex flex-row justify-center">
-                <AccountCardElement
-                    name={props.accountName}
-                    balance={props.accountBalance}
-                    currency={props.accountCurrency}
-                    network={""}
-                    accountType={props.accountType}
-                    closed={props.closed}
-                ></AccountCardElement>
+            <div class="flex flex-row justify-left mb-8 flex-wrap gap-4" >
 
+                <div class="flex flex-row justify-center margin_0_auto">
+                    <AccountCardElement
+                        name={props.accountName}
+                        balance={props.accountBalance}
+                        currency={props.accountCurrency}
+                        network={""}
+                        accountType={props.accountType}
+                        closed={props.closed}
+                    ></AccountCardElement>
+
+                </div>
+                <div class="flex flex-col margin_0_auto">
+                    <div class="mx-auto mt-4">
+                        <label for="password" class="block mb-2 text-sm font-medium">Account Password</label>
+                        <input
+                            value={password}
+                            onChange={(event: any) => setPassword(event.target.value)}
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="••••••••"
+                            class="width-320px border border-gray-300 sm:text-sm rounded-lg focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2.5 dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
+
+                        />
+                    </div>
+                    <button
+                        onClick={payClicked}
+                        class="width-320px mb-4 mt-4 mx-auto text-white bg-indigo-500 hover:bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-indigo-300 rounded-lg text-2xl font-bold px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
+                    >Subscribe</button>
+                    <div class="w-60 mx-auto mt-4 text-center">
+                        <p class={"text-red-600	"}>{errorMessage}</p>
+                    </div>
+                </div>
             </div>
-            <div class="w-60 mx-auto mt-4">
-                <label for="password" class="block mb-2 text-sm font-medium">Account Password</label>
-                <input value={password} onChange={(event: any) => setPassword(event.target.value)} type="password" name="password" id="password" placeholder="••••••••" class="border border-gray-300 sm:text-sm rounded-lg focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2.5 dark:focus:ring-indigo-500 dark:focus:border-indigo-500" />
-            </div>
-            <div class="w-60 mx-auto mt-4 text-center">
-                <p class={"text-red-600	"}>{errorMessage}</p>
-            </div>
-            <button
-                onClick={payClicked}
-                class="w-60 mb-4 mt-4 mx-auto text-white bg-indigo-500 hover:bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-indigo-300 rounded-lg text-2xl font-bold px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Subscribe</button></>}
+        </>}
     </div>
 }
 
