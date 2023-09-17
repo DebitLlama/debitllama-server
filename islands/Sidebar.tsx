4f8fde7e05042b8c90cc5d6ef04b8d55fd1411cc
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-17 17:50:59 +0200

    added new pages

--- islands/Sidebar.tsx
+++ islands/Sidebar.tsx
@@ -109,6 +109,22 @@ export default function SideBar() {
                         <span class="ml-2 text-sm tracking-wide truncate">Relayer</span>
                     </a>
                 </li>
+                <li>
+                    <a href="/app/apiAccess" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                        <span class="inline-flex justify-center items-center ml-4">
+                            <svg style={"margin-left: -2px;"} fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-83 0-141.5-58.5T80-320q0-73 45.5-127.5T240-516v83q-35 12-57.5 43T160-320q0 50 35 85t85 35q50 0 85-35t35-85v-40h235q8-9 19.5-14.5T680-380q25 0 42.5 17.5T740-320q0 25-17.5 42.5T680-260q-14 0-25.5-5.5T635-280H476q-14 69-68.5 114.5T280-120Zm400 0q-56 0-101.5-27.5T507-220h107q14 10 31 15t35 5q50 0 85-35t35-85q0-50-35-85t-85-35q-20 0-37 5.5T611-418L489-621q-21-4-35-20t-14-39q0-25 17.5-42.5T500-740q25 0 42.5 17.5T560-680v8.5q0 3.5-2 8.5l87 146q8-2 17-2.5t18-.5q83 0 141.5 58.5T880-320q0 83-58.5 141.5T680-120ZM280-260q-25 0-42.5-17.5T220-320q0-22 14-38t34-21l94-156q-29-27-45.5-64.5T300-680q0-83 58.5-141.5T500-880q83 0 141.5 58.5T700-680h-80q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 43 26 75.5t66 41.5L337-338q2 5 2.5 9t.5 9q0 25-17.5 42.5T280-260Z" /></svg>
+                        </span>
+                        <span class="ml-2 text-sm tracking-wide truncate">Access & Webhooks</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://debitllama.gitbook.io/debitllama/" target="_blank" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                        <span class="inline-flex justify-center items-center ml-4">
+                            <svg style={"margin-left: -2px;"} fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-280h280v-80H280v80Zm0-160h400v-80H280v80Zm0-160h400v-80H280v80Zm-80 480q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z" /></svg>
+                        </span>
+                        <span class="ml-2 text-sm tracking-wide truncate">Documentation</span>
+                    </a>
+                </li>
 
                 <li class="px-5">
                     <div class="flex flex-row items-center h-8">
