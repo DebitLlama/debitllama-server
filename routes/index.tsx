a6609f6abcd83d2eb9232baf8752c2b86a948680
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-18 13:06:31 +0200

    updated styles for signup and login

--- routes/index.tsx
+++ routes/index.tsx
@@ -56,7 +56,10 @@ export default function Login(props: PageProps) {
                         <h2 class="text-2xl font-bold mb-5 text-center">Welcome to <span>Debit</span><span class="text-gray-600">Llama</span></h2>
                     </div>
 
-                    <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+                    <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 bg-gradient-gray-to-white-variant2">
+                        <div class="mx-auto pt-1 pb-1">
+                            <h2 class="text-2xl text-center text-gray-600">User Login</h2>
+                        </div>
                         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                             {err && (
                                 <div class="bg-red-400 border-l-4 p-4" role="alert">
@@ -65,9 +68,7 @@ export default function Login(props: PageProps) {
                                 </div>
                             )}
                             <form class="space-y-4 md:space-y-6" method="POST">
-                                <div class="mx-auto">
-                                    <h2 class="text-2xl font-bold mb-5 text-center">Login</h2>
-                                </div>
+
                                 <div>
                                     <label for="email" class="block mb-2 text-sm font-medium">Your email</label>
                                     <input type="email" name="email" id="email" class="border border-gray-300 sm:text-sm rounded-lg focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2.5 dark:focus:ring-indigo-500 dark:focus:border-indigo-500" placeholder="name@company.com" />
@@ -77,13 +78,12 @@ export default function Login(props: PageProps) {
                                     <input type="password" name="password" id="password" placeholder="••••••••" class="border border-gray-300 sm:text-sm rounded-lg focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2.5 dark:focus:ring-indigo-500 dark:focus:border-indigo-500" />
                                 </div>
 
-                                <button type="submit" class="w-full text-white bg-indigo-500 hover:bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Login In</button>
+                                <button type="submit" class="w-full text-white bg-indigo-500 hover:bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-2xl px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Log in</button>
                                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                                     Don't have an account yet? <a href="/signup" class="font-medium text-indigo-600 hover:underline dark:text-indigo-500">Sign up</a>
                                 </p>
                             </form>
                         </div>
-
                     </div>
                 </div>
             </section>
--- routes/signup.tsx
+++ routes/signup.tsx
@@ -35,18 +35,16 @@ export default function SignUp(props: PageProps) {
         <title>DebitLlama</title>
         <link rel="stylesheet" href="/styles.css" />
     </Head>
-        <section class="bg-gray-200 fixed w-full h-full overflow-auto">
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto">
-
-
-                <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0" >
+        <section class="bg-gray-200 h-screen">
+            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 " >
                     <div class={"mx-auto"}>
                         <img src="/ridingALlama.png" />
                     </div>
                     <div class="mx-auto">
-                        <h2 class="text-2xl font-bold mb-5 text-center">Sign Up</h2>
+                        <h2 class="text-2xl text-gray-500 mb-5 text-center">Sign up for a Free Account!</h2>
                     </div>
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8 bg-gradient-white-to-gray">
                         {err && (
                             <div class="bg-red-400 border-l-4 p-4" role="alert">
                                 <p class="font-bold">Error</p>
@@ -63,7 +61,7 @@ export default function SignUp(props: PageProps) {
                                 <input type="password" name="password" id="password" placeholder="••••••••" class="border border-gray-300 sm:text-sm rounded-lg focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2.5 dark:focus:ring-indigo-500 dark:focus:border-indigo-500" />
                             </div>
 
-                            <button type="submit" class="w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Sign Up</button>
+                            <button type="submit" class="w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-2xl px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Sign Up</button>
                             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Already have an account? <a href="/" class="font-medium text-indigo-600 hover:underline dark:text-indigo-500">Login here</a>
                             </p>
