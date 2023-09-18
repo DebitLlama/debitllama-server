323a73e655f6204f73e33c585813aa0c71c226d2
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-18 13:07:03 +0200

    updated password UI

--- islands/accountPasswordInput.tsx
+++ islands/accountPasswordInput.tsx
@@ -13,10 +13,17 @@ export interface AccountPasswordInputProps {
 export default function AccountPasswordInput(props: AccountPasswordInputProps) {
     return <> <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="encryptionpass">Virtual Account Password</label>
-        <input value={props.password} onKeyUp={(event: any) => props.setPassword(event.target.value)} onChange={(event: any) => props.setPassword(event.target.value)} required data-lpignore="true" autocomplete={"off"} class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+        <input
+            value={props.password}
+            onKeyUp={(event: any) => props.setPassword(event.target.value)}
+            onChange={(event: any) => props.setPassword(event.target.value)}
+            required
+            data-lpignore="true"
+            autocomplete={"off"}
+            class="w-full px-3 py-2 border-x border-t border-gray-300 rounded-t-md focus:outline-none focus:border-indigo-500"
             type="password" id="encryptionpass" name="encryptionpass" placeholder="********" />
         <PasswordStrengthDisplay passwordStrengthNotification={props.passwordStrengthNotification}></PasswordStrengthDisplay>
-        <p class="text-sm text-gray-500">You need to use a good or strong password</p>
+        <p class="text-sm text-gray-500 py-4 px-2">You need to use a good or strong password!</p>
     </div>
 
         <div class="mb-4">
@@ -25,7 +32,7 @@ export default function AccountPasswordInput(props: AccountPasswordInputProps) {
                 type="password" id="confirm-password" name="confirmpassword" placeholder="********" />
             <p class="text-red-800">{props.passwordMatchError}</p>
         </div>
-     
+
     </>
 }
 
@@ -34,8 +41,7 @@ interface PasswordStrengthDisplayProps {
 }
 
 function PasswordStrengthDisplay(props: PasswordStrengthDisplayProps) {
-    return <div class="bg-white w-full rounded-lg flex flex-col text-sm py-4 px-2 text-gray-500 ">
-        {/* <div class="w-8 text-gray-900">H<span class="text-xs">1</span></div> */}
+    return <div class="bg-white w-full border-gray-300  border-dotted border-2 rounded-b-lg flex flex-col text-md py-4 px-2 text-gray-800 ">
         <div>{props.passwordStrengthNotification === "" ? "Password Strength: None" : props.passwordStrengthNotification}</div>
 
     </div>
