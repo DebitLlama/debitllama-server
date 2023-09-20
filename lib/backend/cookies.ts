e1980473fa7140b08dbb365622c7e1b2e1692e08
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-20 20:50:22 +0200

    cookies setter abstractions

---
+++ lib/backend/cookies.ts
@@ -0,0 +1,47 @@
+import { deleteCookie, setCookie } from "$std/http/cookie.ts";
+import { CookieNames } from "../enums.ts";
+
+export function setProfileRedirectCookie(headers: Headers, value: string) {
+  setCookie(headers, {
+    name: CookieNames.profileRedirect,
+    value: value,
+    maxAge: 600, // Expires in 10 minutes
+  });
+}
+
+export function deleteProfileRedirectCookie(headers: Headers) {
+  deleteCookie(headers, CookieNames.profileRedirect);
+}
+
+export function setSupaloginCookie(
+  headers: Headers,
+  value: string,
+  maxAge: number,
+) {
+  setCookie(headers, {
+    name: CookieNames.supaLogin,
+    value,
+    maxAge,
+  });
+}
+
+export function setRenderSidebarOpen(
+  headers: Headers,
+  value: string,
+  maxAge: number,
+) {
+  setCookie(headers, {
+    name: CookieNames.renderSidebarOpen,
+    value,
+    maxAge,
+  });
+}
+
+export function setLoginRedirect(headers: Headers, url: URL) {
+  setCookie(headers, {
+    name: CookieNames.loginRedirect,
+    value: url.pathname + url.search,
+    maxAge: 600, // 10 minutes max,
+    path: "/",
+  });
+}
