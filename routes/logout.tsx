47f63148cd897df3b065e9e90b077205e6cbece5
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-20 20:57:36 +0200

    updated cookies

--- routes/logout.tsx
+++ routes/logout.tsx
@@ -1,11 +1,12 @@
 import { Handlers } from "$fresh/server.ts";
 import { deleteCookie } from "$std/http/cookie.ts";
+import { CookieNames } from "../lib/enums.ts";
 
 export const handler: Handlers = {
   GET(_req, _ctx) {
     const headers = new Headers();
-    deleteCookie(headers, "supaLogin");
-    deleteCookie(headers, "renderSidebarOpen");
+    deleteCookie(headers, CookieNames.supaLogin);
+    deleteCookie(headers, CookieNames.renderSidebarOpen);
     headers.set("location", "/");
     return new Response(null, {
       status: 303,
