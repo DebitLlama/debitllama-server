782d999e9bf29ca029c1d8d0fc698276db931a4e
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-20 20:59:40 +0200

    feedback link refactor

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -27,10 +27,12 @@ export default function Layout(props: LayoutProps) {
           class="border-grey-200 bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
           <div class="pt-4 text-center text-neutral-700 dark:text-neutral-200 flex flex-col">
             © ZKP Tech Solutions Ltd 2023{" "}
-            <a
-              class="text-gray-500 hover:text-gray-700 py-1 border-gray-500"
-              href="/app/feedback"
-            >Feedback</a>
+            <div >
+              <a
+                class="w-64 text-gray-500 hover:text-gray-700 py-1 border-gray-500"
+                href="/app/feedback"
+              >Feedback</a>
+            </div>
           </div>
         </footer>
       </div>
