f27ac65b9054dfdeaa64ae92e143af520ab5c22f
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-25 06:55:04 +0200

    added access tokens page

--- routes/app/apiAccess.tsx
+++ routes/app/apiAccess.tsx
@@ -1,46 +1,85 @@
 // deno-lint-ignore-file no-explicit-any
 import { Handlers, PageProps } from "$fresh/server.ts";
 import Layout from "../../components/Layout.tsx";
+import AccessTokenUISwitcher from "../../islands/AccessTokenUISwitcher.tsx";
+import { generateApiAuthToken } from "../../lib/backend/auth.ts";
+import { getTotalPages } from "../../lib/backend/businessLogic.ts";
 import QueryBuilder from "../../lib/backend/queryBuilder.ts";
+import { errorResponseBuilder, successResponseBuilder } from "../../lib/backend/responseBuilders.ts";
+import { ApiAccessErrors, TokenExpiry, monthsToDate } from "../../lib/enums.ts";
 import { State } from "../_middleware.ts";
+
 export const handler: Handlers<any, State> = {
     async POST(req, ctx) {
         const form = await req.formData();
-        const subject = form.get("subject") as string;
-        const message = form.get("message") as string;
-        const queryBuilder = new QueryBuilder(ctx);
-        const insert = queryBuilder.insert();
-        const { error } = await insert.Feedback.newFeedback(subject, message);
-        const headers = new Headers();
+        const expiry = form.get("expiry") as string;
 
-        if (error) {
-            headers.set("location", `/app/feedback?error=${"Unable to save feedback"}`)
-        } else {
-            headers.set("location", `/app/feedback?success=${"Message sent!"}`)
+        const calculatedExpiryDate = monthsToDate[expiry as TokenExpiry];
+
+        if (calculatedExpiryDate === undefined) {
+            return errorResponseBuilder(ApiAccessErrors.InvalidExpiryDate)
         }
 
-        return new Response(null, { status: 303, headers })
+        const queryBuilder = new QueryBuilder(ctx);
 
+        const insert = queryBuilder.insert();
+        const accessToken = generateApiAuthToken();
+        await insert.ApiAuthTokens.newToken(
+            accessToken,
+            calculatedExpiryDate()
+        ).catch((err) => {
+            // If the access_token is not unique for some reason, or the insert fails I have an error here
+        })
+
+        const select = queryBuilder.select();
+        const { data: accesstokens, error, count } = await select.ApiAuthTokens.byUseridPaginated(
+            "created_at",
+            false,
+            0,
+            9
+        );
+
+        const totalPages = getTotalPages(count, 10);
+
+        const { data: webhookdata, error: WebhooksErr } = await select.Webhooks.byUserId();
+
+        const webhook_url = webhookdata.length === 0 ? "" : webhookdata[0].webhook_url;
+
+        return ctx.render({ ...ctx.state, accesstokens, totalPages, webhook_url })
     },
 
-    GET(_req, ctx) {
-        return ctx.render({ ...ctx.state })
+    async GET(_req, ctx) {
+        const queryBuilder = new QueryBuilder(ctx);
+        const select = queryBuilder.select();
+        const { data: accesstokens, error, count } = await select.ApiAuthTokens.byUseridPaginated(
+            "created_at",
+            false,
+            0,
+            9
+        );
+
+        const totalPages = getTotalPages(count, 10);
+
+        const { data: webhookdata, error: WebhooksErr } = await select.Webhooks.byUserId();
+
+        const webhook_url = webhookdata.length === 0 ? "" : webhookdata[0].webhook_url;
+
+        return ctx.render({ ...ctx.state, accesstokens, totalPages, webhook_url })
+    },
+    async DELETE(_req, ctx) {
+        const queryBuilder = new QueryBuilder(ctx);
+        const deleteQ = queryBuilder.delete();
+        const json = await _req.json();
+        const accesstoken = json.accesstoken as string;
+
+        await deleteQ.ApiAuthTokens.ByAccessToken(accesstoken);
+
+        return successResponseBuilder("ok");
     }
 }
 
-export default function Feedback(props: PageProps) {
-    const err = props.url.searchParams.get("error");
-    const success = props.url.searchParams.get("success");
-
+export default function ApiAccess(props: PageProps) {
     return <Layout renderSidebarOpen={props.data.renderSidebarOpen} isLoggedIn={props.data.token}>
-        <div class="mt-10 px-5 mx-auto flex max-w-screen-md flex-col justify-center">
-            <div class="mx-auto text-center">
-                <h1 class="text-xl" >(WORK IN PROGRESS)</h1>
-                <h2 class="text-lg" >API ACCESS </h2>
-
-                <p class="text-sm">ACCESS TOKENS AND WEBHOOK ENDPOINT SETTINGS GO HERE</p>
-
-            </div>
-        </div>
+        <AccessTokenUISwitcher data={props.data}></AccessTokenUISwitcher>
     </Layout>
 }
