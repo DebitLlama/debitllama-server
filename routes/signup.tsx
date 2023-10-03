e92d6ce8b477fbefe0f912e7f79929bb2fda1d10
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-10-03 19:36:47 +0200

    updated link

--- routes/signup.tsx
+++ routes/signup.tsx
@@ -63,7 +63,7 @@ export default function SignUp(props: PageProps) {
 
                             <button type="submit" class="w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-2xl px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Sign Up</button>
                             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Already have an account? <a href="/" class="font-medium text-indigo-600 hover:underline dark:text-indigo-500">Login here</a>
+                                Already have an account? <a href="/login" class="font-medium text-indigo-600 hover:underline dark:text-indigo-500">Login here</a>
                             </p>
                         </form>
                     </div>
