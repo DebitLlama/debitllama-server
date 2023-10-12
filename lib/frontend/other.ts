f34d47778cf636c08a67ebc4ef700b1a695faa1d
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-10-12 16:22:53 +0200

    cookie banner and privacy policy

--- lib/frontend/other.ts
+++ lib/frontend/other.ts
@@ -3,3 +3,11 @@
 export function setSidebarOpenCookie(to: string) {
   document.cookie = `renderSidebarOpen=${to};path=/`;
 }
+
+export function setCookiesAcceptedToSS() {
+  window.localStorage.setItem("cookiesAccepted", "true");
+}
+
+export function getCookiesAcceptedFromSS() {
+  return window.localStorage.getItem("cookiesAccepted") === "true";
+}
---
+++ routes/privacyPolicy.tsx
@@ -0,0 +1,26 @@
+export default function PrivacyPolicy() {
+    return <div class="flex flex-row justify-center">
+        <div class="border shadow-xl h-full flex flex-col justify-center max-w-4xl	p-5">
+            <h2 class={"text-center mt-5 mb-5 text-3xl"}>Privacy Policy</h2>
+            <p>At DebitLlama.com, we are committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy outlines our practices regarding the collection, use, and disclosure of your personal data when you visit our website.</p>
+            <h3 class="mt-5 mb-5 text-lg">1. Information We Collect</h3>
+            <p>We collect personally identifiable information (PII) from our users after they sign up to DebitLlama.com for regulatory compliance. We do not share any of this information with third parties unless requested by law enforcement.</p>
+            <h3 class="mt-5 mb-5 text-lg">2. Cookies</h3>
+            <p>DebitLlama.com uses functional cookies to ensure that the website functions correctly. These cookies are necessary for the performance and accessibility of our site. They do not collect any personal information, and they are not used for tracking or monitoring your online activities.</p>
+            <h3 class="mt-5 mb-5 text-lg">3. Third-Party Services</h3>
+            <p>We do not use third-party services or tools that may collect your personal data while you browse DebitLlama.com. We do not share any information with third parties for marketing or advertising purposes.</p>
+            <h3 class="mt-5 mb-5 text-lg">4. Data Security</h3>
+            <p>We take the security of your information seriously. Our website is designed to protect your data from unauthorized access, alteration, disclosure, or destruction. However, please note that no method of data transmission over the internet is completely secure, and we cannot guarantee the security of information transmitted to us electronically.</p>
+            <h3 class="mt-5 mb-5 text-lg">5. Children's privacy</h3>
+            <p>DebitLlama.com is not intended for use by children under the age of 13. We do not knowingly collect any personal information from children. If you believe that we may have collected information from a child under the age of 13, please contact us, and we will promptly remove the information from our records.</p>
+            <h3 class="mt-5 mb-5 text-lg">6. GDPR Compliance and Opt-Out</h3>
+            <p>DebitLlama.com fully complies with the General Data Protection Regulation (GDPR). If you are a resident of the European Union, you have the right to access, rectify, or erase your personal data. You can also opt out of any data collection and processing on our website. To exercise these rights or to opt out, please contact us at support@debitllama.com.</p>
+            <h3 class="mt-5 mb-5 text-lg">7. Changes to this Privacy Policy</h3>
+            <p>We reserve the right to update or modify this Privacy Policy at any time without prior notice. Any changes will be effective immediately upon posting on this page. It is your responsibility to review this Privacy Policy periodically for updates.</p>
+            <h3 class="mt-5 mb-5 text-lg">8. Contact Us</h3>
+            <p>If you have any questions or concerns regarding our Privacy Policy, GDPR compliance, or the handling of your personal information, please contact us at [contact email address].
+                By using DebitLlama.com, you consent to the terms outlined in this Privacy Policy. If you do not agree with these terms, please refrain from using our website.
+                Thank you for choosing DebitLlama.com. Your privacy is important to us, and we are dedicated to providing you with a secure and enjoyable online experience while respecting your GDPR rights.</p>
+        </div>
+    </div>
+}
