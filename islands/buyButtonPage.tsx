aa52675470d76b5dfc2d122562b37e991b4ad5ef
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-10-20 08:24:40 +0200

    added link to terms and conditions

--- islands/buyButtonPage.tsx
+++ islands/buyButtonPage.tsx
@@ -1042,6 +1042,7 @@ function LoggedOutUi(props: LoggedOutUiProps) {
             </div>
 
             <button type="submit" class="w-full text-white bg-indigo-500 hover:bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-2xl px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Log In</button>
+            <p class="text-sm" >By logging in you accept the <a href="/termsAndConditions" class="text-sm text-indigo-500">Terms and conditions</a></p>
             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don't have an account yet? <a target="_blank" href="/signup" class="font-medium text-indigo-600 hover:underline dark:text-indigo-500">Sign up</a>
             </p>
