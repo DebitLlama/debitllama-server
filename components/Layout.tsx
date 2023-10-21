f5ca3a81517529f45a5db139489400fdcbc4d2d2
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-10-21 05:40:46 +0200

    Moved feedback button to sidebar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -23,20 +23,7 @@ export default function Layout(props: LayoutProps) {
       <SideBar renderSidebarOpen={props.renderSidebarOpen}></SideBar>
       <div class="mx-auto layoutheight overflow-auto" id="layout-children">
         {props.children}
-        <footer
-          class="border-grey-200 bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
-          <div class="pt-4 text-center text-neutral-700 dark:text-neutral-200 flex flex-col">
-            © ZKP Tech Solutions Ltd 2023{" "}
-            <div >
-              <a
-                class="w-64 text-gray-500 hover:text-gray-700 py-1 border-gray-500"
-                href="/app/feedback"
-              >Feedback</a>
-            </div>
-          </div>
-        </footer>
       </div>
-
     </>
   );
 }
