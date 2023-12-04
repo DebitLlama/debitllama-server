392095996fad9072899007cdeb96150b2257c10a
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-12-04 00:36:54 +0000

    email verified page added

---
+++ routes/emailVerified.tsx
@@ -0,0 +1,8 @@
+export default function EmailVerified() {
+    return <div class="w-full max-w-sm mx-auto bg-white p-8 rounded-md shadow-md">
+        <h1 class="text-2xl font-bold mb-6 text-center">Email Verified 🚀</h1>
+        <div class="flex flex-row justify-center">
+            <a class="mx-auto text-xl text-indigo-800" href="/login">Go to Login</a>
+        </div>
+    </div>
+}
