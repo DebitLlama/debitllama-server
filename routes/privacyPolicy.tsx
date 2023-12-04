b3a5ef69e834ad68d4a169560d3c87f1a545959e
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-12-04 22:52:24 +0000

    added email to privacy policy

--- routes/privacyPolicy.tsx
+++ routes/privacyPolicy.tsx
@@ -18,7 +18,7 @@ export default function PrivacyPolicy() {
             <h3 class="mt-5 mb-5 text-lg">7. Changes to this Privacy Policy</h3>
             <p>We reserve the right to update or modify this Privacy Policy at any time without prior notice. Any changes will be effective immediately upon posting on this page. It is your responsibility to review this Privacy Policy periodically for updates.</p>
             <h3 class="mt-5 mb-5 text-lg">8. Contact Us</h3>
-            <p>If you have any questions or concerns regarding our Privacy Policy, GDPR compliance, or the handling of your personal information, please contact us at [contact email address].
+            <p>If you have any questions or concerns regarding our Privacy Policy, GDPR compliance, or the handling of your personal information, please contact us at info.zkptech@pm.me.
                 By using DebitLlama.com, you consent to the terms outlined in this Privacy Policy. If you do not agree with these terms, please refrain from using our website.
                 Thank you for choosing DebitLlama.com. Your privacy is important to us, and we are dedicated to providing you with a secure and enjoyable online experience while respecting your GDPR rights.</p>
         </div>
