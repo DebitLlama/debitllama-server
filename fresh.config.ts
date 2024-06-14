8559e04842cd7be7b42d3f67b7dc05666192740e
StrawberryChocolateFudge <hopeter@protonmail.com>
2024-06-14 19:12:51 +0200

    devops now uses Pup and no more nodejs

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "$fresh/server.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
+
+const port = parseInt(Deno.env.get("PUP_CLUSTER_PORT") || "3000", 10);
+
 export default defineConfig({
   plugins: [twindPlugin(twindConfig)],
-  port: 3000,
+  port,
 });
