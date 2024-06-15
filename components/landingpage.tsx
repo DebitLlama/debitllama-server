5c741010841c7d52f2f262f1a49997b4b364c801
StrawberryChocolateFudge <hopeter@protonmail.com>
2024-06-15 13:41:15 +0200

    updated landing page

--- components/landingpage.tsx
+++ components/landingpage.tsx
@@ -35,7 +35,8 @@ export function SimpleLandingPage() {
             </div>
         </div>
         <InfoSection></InfoSection>
-        <FeesInfoSection></FeesInfoSection>
+        {/* <FeesInfoSection></FeesInfoSection> */}
+        <DownloadLinks></DownloadLinks>
         <SimpleCheckoutFlow></SimpleCheckoutFlow>
         <FooterMarquee></FooterMarquee>
     </div>
@@ -126,6 +127,31 @@ export function FeesInfoSection() {
             </div>
         </div>
     </section>
+}
+
+export function DownloadLinks() {
+    return <div class="bg-white">
+        <div class="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+            <div class="relative isolate overflow-hidden bg-gray-200 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+                <svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
+                    <circle cx="512" cy="512" r="512" fill="#3730a3 " fill-opacity="0.7" />
+                    <defs>
+                        <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                            <stop stop-color="#7775D6" />
+                            <stop offset="1" stop-color="#E935C1" />
+                        </radialGradient>
+                    </defs>
+                </svg>
+                <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+                    <h2 class="text-3xl font-bold tracking-tight text-black sm:text-4xl">Direct Debit Payments<br />Start using our app today.</h2>
+                    <p class="mt-6 text-lg leading-8 text-black-300">Set up an account once and approve payments from it using any device. Keep using metamask, use a password based wallet abstraction or turn a cutting-edge passkey into a wallet.</p>
+                    <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start mb-10">
+                        <a href="https://debitllama.gitbook.io/debitllama/process-payments-via-solvers" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Start accepting payments by running a solver</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 
 export function FooterMarquee() {
